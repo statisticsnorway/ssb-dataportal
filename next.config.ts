c6b391122373bf8bbd2622fd0afa085e09143cdd
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-10-24 11:11:29 +0200

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,39 @@
 import type { NextConfig } from "next";
+import path from 'path';
 
 const nextConfig: NextConfig = {
   /* config options here */
+    webpack(config: { module: { rules: any[]; }; resolve: { alias: any; }; }) {
+    // SVG loader config
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        resourceQuery: { not: /url/ },
+        use: ['@svgr/webpack'],
+      }
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    // Aliases
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@global-css': path.resolve(__dirname, 'app/global.css')
+    };
+
+    return config;
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  output: "standalone",
 };
 
 export default nextConfig;
