869f0716517717eea62f6c1110f7abc1f9d9e391
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-10-28 08:37:35 +0100

    add utils

---
+++ utils/constants.ts
@@ -0,0 +1,19 @@
+export const VARDEF_PUBLIC_HOST = 'https://vardef.intern.test.ssb.no/public/variable-definitions';
+export const ID = 'ID';
+export const NB = 'nb';
+export const ACCEPT_LANGUAGE = 'accept-language';
+
+export const KLASS_NAIS_HOST = 'https://klass.intern.test.ssb.no/api/klass/v1/';
+export const KLASS_HOST = 'https://data.ssb.no/api/klass/v1/';
+export const CLASSIFICATIONS = 'classifications';
+export const CLASSIFICATION_FAMILIES = 'classificationfamilies';
+export const API_CLASSIFICATIONS ='api/classifications';
+export const API_CLASSIFICATION_FAMILIES='api/classification-families';
+export const API_VARIABLE_DEFINITIONS='api/variable-definitions';
+
+
+export const filter = {
+    sortNameAsc: "Navn (A–Z)",
+    sortNameDesc: "Navn (Z–A)",
+    sortLastUpdated: "Sist oppdatert",
+}
---
+++ utils/header.ts
@@ -0,0 +1,14 @@
+
+export const parsePreferredLanguage = (headerValue: string): string => {
+    if (!headerValue) return 'nb';
+    const languages = headerValue
+        .split(',')
+        .map((lang) => lang.split(';')[0].trim().toLowerCase()) 
+        .map((lang) => lang.slice(0, 2));
+        
+        for (const lang of languages) {
+            return lang
+        }
+
+    return 'nb';
+}
