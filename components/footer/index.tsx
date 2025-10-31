496c485989e17286c2d564d6cca001b2ba41ac90
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-10-31 12:54:52 +0100

    simplify footer

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,60 +1,26 @@
 import { FC } from 'react';
 
 import styles from './footer.module.css';
-import EmailIcon from './images/email.svg';
-import MetadataLink from '../link';
-import { localization } from '@/lib/language/localization';
+import Link from 'next/link';
 
 export interface FooterProps {
-  /**
-   * font color
-   * @type {string}
-   */
   fontColor?: string;
-  /**
-   * background color
-   * @type {string}
-   */
   backgroundColor?: string;
 }
 
 export const Footer: FC<FooterProps> = ({ fontColor, backgroundColor }) => (
   <footer
     className={styles.footer}
-    style={{ ...(fontColor ? { color: fontColor } : {}), ...(backgroundColor ? { background: backgroundColor } : {}) }}
   >
-    <div className={styles.content}>
-      <div className={styles.column}>Bygget på Digitaliseringsdirektoratet Felles datakatalog</div>
-      <div className={styles.column}>
-        <MetadataLink
-          href='https://www.digdir.no/om-oss/personvernerklaering/706'
-          external={true}
-        >
-          {localization.footer.privacyStatement}
-        </MetadataLink>
-        <MetadataLink
-          href='https://www.digdir.no/om-oss/informasjonskapsler/707'
-          external={true}
-        >
-          {localization.footer.cookies}
-        </MetadataLink>
-        <MetadataLink
-          href='https://uustatus.no/nb/erklaringer/publisert/8020b962-b706-4cdf-ab8b-cdb5f480a696'
-          external={true}
-        >
-          {localization.footer.accessibility}
-        </MetadataLink>
-      </div>
-      <div className={styles.column}>
-        <MetadataLink
-          href='mailto:metadata@ssb.no'
-          icon={<EmailIcon />}
-        >
+    <section className={styles.footerInfoSection}>
+      Bygget på Digitaliseringsdirektoratet Felles datakatalog
+    </section>
+    <section className={styles.footerLinkSection}>
+      <Link
+        href='mailto:metadata@ssb.no'>
           metadata@ssb.no
-        </MetadataLink>
-      </div>
-    </div>
+      </Link>
+    </section>
   </footer>
 );
 
-export default Footer;
