5a60c3e5cdc8cea2ab09acebfee62815c8b7b397
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-10-31 12:54:18 +0100

    test service page layout

--- app/(services)/classifications/page.tsx
+++ app/(services)/classifications/page.tsx
@@ -1,5 +1,14 @@
+import { SearchHitsLayout } from "@/components/search-hits-layout";
+
 export default function Classifications() {
     return (
-        <h1>Hallo klasser</h1>
+        <SearchHitsLayout
+            infoContent={<h1>Hallo info</h1>}
+            mainContent={<h1>Hallo main</h1>}
+            filterContent={<h1>Hallo filter</h1>}
+        >
+            <h1>Hallo klasser</h1>
+        </SearchHitsLayout>
+    
     )
 }
--- app/(services)/variable-definitions/page.tsx
+++ app/(services)/variable-definitions/page.tsx
@@ -1,7 +1,13 @@
+import { SearchHitsLayout } from "@/components/search-hits-layout";
+
 export default function VariableDefinitions() {
     return (
-        <>
+        <SearchHitsLayout
+        infoContent={<h1>Hallo info</h1>}
+        mainContent={<h1>Hallo main</h1>}
+        filterContent={<h1>Hallo filter</h1>}
+        >
             <h1>Hallo vars</h1>
-        </>
+        </SearchHitsLayout>
     )
 }
