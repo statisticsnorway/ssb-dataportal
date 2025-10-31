a1bce66b2566378b5ef905c258771455f304ffa5
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-10-31 17:08:31 +0100

    add mock code jest test

--- jest.setup.ts
+++ jest.setup.ts
@@ -7,6 +7,8 @@ jest.mock('react-localization', () => ({
         strings: any;
         header: { menu: string; contactUs: string; };
         footer: { privacyStatement: string; accessibility: string; };
+        concept: { noName: string };
+        currentLanguage: string;
         constructor(strings: any) {
             this.strings = strings;
             this.header = {
@@ -17,8 +19,16 @@ jest.mock('react-localization', () => ({
                 privacyStatement: 'Personvernerklæring',
                 accessibility: 'Tilgjengelighet',
             };
+            this.concept = {
+                noName: 'Uten navn', // 👈 add this
+            };
+            this.currentLanguage = 'nb';
         }
         setLanguage(lang: string) {
+            this.currentLanguage = lang;
+        }
+        getLanguage() {
+            return this.currentLanguage;
         }
     },
     LocalizedStringsMethods: class {},
