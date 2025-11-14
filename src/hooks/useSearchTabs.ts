3dd609e95e7c050279257582c6ab121fdac3d7ea
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-11-14 10:08:53 +0100

    do not set state in useEffect

--- src/hooks/useSearchTabs.ts
+++ src/hooks/useSearchTabs.ts
@@ -1,29 +1,40 @@
 'use client';
 
 import { useRouter, usePathname } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export function useSearchTabs() {
     const router = useRouter();
     const pathname = usePathname();
     const [selectedTab, setSelectedTab] = useState('');
-    const getTabFromPath = (pathname: string) => {
-        if (pathname.includes('/classifications')) return 'klassTab';
-        if (pathname.includes('/variable-definitions')) return 'vardefTab';
-        return '';
+
+    const tabChangedByUser = useRef(false);
+
+    const handleTabChange = (value: string) => {
+        tabChangedByUser.current = true;
+        setSelectedTab(value);
+
+        if (value === 'klassTab') router.push('/classifications');
+            else if (value === 'vardefTab') router.push('/variable-definitions');
     };
 
     useEffect(() => {
+        if (tabChangedByUser.current) {
+            tabChangedByUser.current = false;
+            return;
+        }
+
         const newTab = getTabFromPath(pathname);
         if (newTab !== selectedTab) {
             setSelectedTab(newTab);
         }
     }, [pathname, selectedTab]);
 
-    const handleTabChange = (value: string) => {
-        setSelectedTab(value);
-        if (value === 'klassTab') router.push('/classifications');
-        else if (value === 'vardefTab') router.push('/variable-definitions');
+
+    const getTabFromPath = (pathname: string) => {
+        if (pathname.includes('/classifications')) return 'klassTab';
+        if (pathname.includes('/variable-definitions')) return 'vardefTab';
+        return '';
     };
 
     return { selectedTab, handleTabChange };
