3971a530747d110ede3ca3c9c8a2289f083a12fc
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-11-21 12:28:45 +0100

    rename for readability

--- src/app/(services)/layout.tsx
+++ src/app/(services)/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 import SearchPage from '@/components/search-page/searchPage';
 import styles from './services.module.css';
 
-export default async function TabsLayout({ children }: { children: ReactNode }) {
+export default async function ServicesLayout({ children }: { children: ReactNode }) {
   return (
     <div className={styles.metadataContainer}>
       <SearchPage>{children}</SearchPage>
