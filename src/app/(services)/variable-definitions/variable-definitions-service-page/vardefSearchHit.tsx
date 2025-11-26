a56cfb978ee9f858232bfc0ac585a78d0602ea8e
Jan <63044278+JanhSander@users.noreply.github.com>
2025-11-26 15:18:31 +0100

    Used card for search hit

--- src/app/(services)/variable-definitions/variable-definitions-service-page/vardefSearchHit.tsx
+++ src/app/(services)/variable-definitions/variable-definitions-service-page/vardefSearchHit.tsx
@@ -1,4 +1,4 @@
-import { Paragraph } from '@digdir/designsystemet-react';
+import {Card, Heading, Paragraph} from '@digdir/designsystemet-react';
 import { SearchHit } from '@/components/search-hit';
 import { localization } from '@/libs/language';
 import { VariableDefinitionType } from '@/types/variableDefinition';
@@ -9,24 +9,12 @@ interface VardefSearchHitProps {
 
 const VardefSearchHit = ({ variableDefinition }: VardefSearchHitProps) => {
   return (
-    <SearchHit
-      key={variableDefinition.id}
-      title={variableDefinition.name}
-      titleHref={`/variable-definitions/${variableDefinition.id}`}
-      content={
-        <div>
-          <section>
-            <Paragraph>
-              <span>{localization.id}</span> -<span>{variableDefinition.id}</span>
-            </Paragraph>
-            <Paragraph>
-              <span>{localization.lastModified}</span> -<span>{variableDefinition.last_updated_at}</span>
-            </Paragraph>
-          </section>
-          <Paragraph>{variableDefinition.definition}</Paragraph>
-        </div>
-      }
-    />
+        <Card>
+            <Heading data-size="2xl" level={2}>
+                {variableDefinition.name} - {variableDefinition.short_name}
+            </Heading>
+            <Paragraph>{variableDefinition.definition}</Paragraph>
+        </Card>
   );
 };
 
