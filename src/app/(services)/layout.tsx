dcc113a6106b821da1f38e265317d34bb597b2df
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-11-27 19:36:35 +0100

    remove layout layer

--- src/app/(services)/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from 'react';
-import styles from './services.module.css';
-
-export default async function ServicesLayout({ children }: { children: ReactNode }) {
-  return <div className={styles.metadataContainer}>{children}</div>;
-}
