ef5e4bba83e115edfe3804fad22ba2e31fc4a6e0
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-12-01 15:10:58 +0100

    remove unused section

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import { Heading, Link } from '@digdir/designsystemet-react';
+import { Link } from '@digdir/designsystemet-react';
 import { HouseIcon } from '@navikt/aksel-icons';
 import { FC } from 'react';
-import styles from './header.module.scss';
+import styles from './header.module.css';
 
 export interface HeaderProps {
   homeUrl?: string;
@@ -16,8 +16,6 @@ export const Header: FC<HeaderProps> = ({ homeUrl }) => {
         <Link href={homeUrl} title='Gå til hovedsiden' className={styles.logo}>
           <HouseIcon aria-label='Hjem' fontSize='4rem' />
         </Link>
-        {/* TODO(): This could be section for user avatar. If not remove*/}
-        <section className={styles.userSection}></section>
       </div>
     </header>
   );
