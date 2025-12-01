e1f5cac246f9903eaf94a48113e3c2b750ca743b
Jorgen-5 <rlj@ssb.no>
2025-12-01 16:47:13 +0100

    Update code

--- src/components/variable-details/references.tsx
+++ src/components/variable-details/references.tsx
@@ -14,35 +14,35 @@ export const References = ({ data }: Props) => {
         <InfoPanel>
           <div>
             {data.classificationReference ? (
-              <Field label='Klassifikasjon' value={data.classificationReference} />
+              <Field label='Kodeverkets URI' value={data.classificationReference} />
             ) : (
               <Paragraph data-size='sm'>—</Paragraph>
             )}
           </div>
           <div>
             {data.unitTypes && data.unitTypes.length > 0 ? (
-              <Field label='Enhetstype' value={data.unitTypes.join(', ')} />
+              <Field label='Enhetstyper' value={data.unitTypes.join(', ')} />
             ) : (
               <Paragraph data-size='sm'>—</Paragraph>
             )}
           </div>
           <div>
             {data.subjectFields && data.subjectFields.length > 0 ? (
-              <Field label='Statistikkområde' value={data.subjectFields.join(', ')} />
+              <Field label='Statistikkområder' value={data.subjectFields.join(', ')} />
             ) : (
               <Paragraph data-size='sm'>—</Paragraph>
             )}
           </div>
           <div>
             {data.externalReferenceUri ? (
-              <Field label='External Reference URI' value='Lenke' href={data.externalReferenceUri} />
+              <Field label='URI til ekstern referanse' value='Lenke' href={data.externalReferenceUri} />
             ) : (
               <Paragraph data-size='sm'>—</Paragraph>
             )}
           </div>
           <div>
             {data.relatedVariableDefinitionUris && data.relatedVariableDefinitionUris.length > 0 ? (
-              <Field label='Related Variable URIs' value='Lenke' href={data.relatedVariableDefinitionUris.join(', ')} />
+              <Field label='URI til relevante variabeldefinisjoner' value='Lenke' href={data.relatedVariableDefinitionUris.join(', ')} />
             ) : (
               <Paragraph data-size='sm'>—</Paragraph>
             )}
@@ -53,8 +53,8 @@ export const References = ({ data }: Props) => {
       <Section title="Personopplysninger">
         <InfoPanel>
           <Field
-            label='Inneholder personopplysninger'
-            value={data.containsSpecialCategoriesOfPersonalData ? 'true' : 'false'}
+            label='Inneholder særlige kategorier av personopplysninger'
+            value={data.containsSpecialCategoriesOfPersonalData ? 'Ja' : 'Nei'}
           />
         </InfoPanel>
       </Section>
