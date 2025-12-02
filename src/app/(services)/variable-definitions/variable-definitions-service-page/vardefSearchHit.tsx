84aa45bc2adc354f45e06eb4e399677611ae6e5b
Jan <63044278+JanhSander@users.noreply.github.com>
2025-12-02 15:24:17 +0100

    Yarn lint

--- src/app/(services)/variable-definitions/variable-definitions-service-page/vardefSearchHit.tsx
+++ src/app/(services)/variable-definitions/variable-definitions-service-page/vardefSearchHit.tsx
@@ -13,10 +13,7 @@ const VardefSearchHit = ({ variableDefinition }: VardefSearchHitProps) => {
   return (
     <Card>
       <Heading data-size='xl' level={2}>
-        <Link
-          href={`/variable-definitions/${variableDefinition.id}`}
-          className={styles.vardefSearchHitHeadingLink}
-        >
+        <Link href={`/variable-definitions/${variableDefinition.id}`} className={styles.vardefSearchHitHeadingLink}>
           <span className={styles.vardefSearchHitName}>{variableDefinition.name.nb}</span>
           {variableDefinition.shortName && (
             <span className={styles.vardefSearchHitShortName}>{variableDefinition.shortName}</span>
