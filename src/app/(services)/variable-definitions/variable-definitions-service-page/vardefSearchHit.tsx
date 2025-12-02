ac4125fa6641b354d218f9cca9b4b26b9fd73565
Jan <63044278+JanhSander@users.noreply.github.com>
2025-12-02 11:03:22 +0100

    Separate css for variable-definitions. Global variable for shortname-color. Removed unnessesary style. Formatted code.

--- src/app/(services)/variable-definitions/variable-definitions-service-page/vardefSearchHit.tsx
+++ src/app/(services)/variable-definitions/variable-definitions-service-page/vardefSearchHit.tsx
@@ -1,42 +1,43 @@
-import { Card, Heading, Paragraph } from '@digdir/designsystemet-react';
+import {Card, Heading, Paragraph} from '@digdir/designsystemet-react';
 import Link from 'next/link';
-import { TagsGroup } from '@/components/tags-group';
-import { SUBJECT_FIELD_LOOKUP } from '@/libs/data/subjectFieldLookup';
-import { CompleteResponse } from '@/libs/data-access/variable-definitions/internal';
+import {TagsGroup} from '@/components/tags-group';
+import {SUBJECT_FIELD_LOOKUP} from '@/libs/data/subjectFieldLookup';
+import {CompleteResponse} from '@/libs/data-access/variable-definitions/internal';
+import styles from '../variable-definitions.module.css'
 
 interface VardefSearchHitProps {
-  variableDefinition: CompleteResponse;
+    variableDefinition: CompleteResponse;
 }
 
-const VardefSearchHit = ({ variableDefinition }: VardefSearchHitProps) => {
-  return (
-    <Card>
-      <Heading data-size='xl' level={1}>
-        <Link
-          href={`/variable-definitions/${variableDefinition.id}`}
-          rel='noopener noreferrer'
-          className='vardef-search-hit__heading-link'
-        >
-          <span className='vardef-search-hit__name'>{variableDefinition.name.nb}</span>
-          {variableDefinition.shortName && (
-            <span className='vardef-search-hit__short-name'>{variableDefinition.shortName}</span>
-          )}
-        </Link>
-      </Heading>
-      <Paragraph className='clamp'>{variableDefinition.definition.nb}</Paragraph>
-      <TagsGroup
-        maxTags={4}
-        tagsData={
-          new Set(
-            (variableDefinition.subjectFields ?? []).map((field) => ({
-              key: field,
-              title: SUBJECT_FIELD_LOOKUP[field] ?? field,
-            })),
-          )
-        }
-      />
-    </Card>
-  );
+const VardefSearchHit = ({variableDefinition}: VardefSearchHitProps) => {
+    return (
+        <Card>
+            <Heading data-size='xl' level={2}>
+                <Link
+                    href={`/variable-definitions/${variableDefinition.id}`}
+                    rel='noopener noreferrer'
+                    className={styles.vardefSearchHitHeadingLink}
+                >
+                    <span className={styles.vardefSearchHitName}>{variableDefinition.name.nb}</span>
+                    {variableDefinition.shortName && (
+                        <span className={styles.vardefSearchHitShortName}>{variableDefinition.shortName}</span>
+                    )}
+                </Link>
+            </Heading>
+            <Paragraph className={styles.truncateLines}>{variableDefinition.definition.nb}</Paragraph>
+            <TagsGroup
+                maxTags={4}
+                tagsData={
+                    new Set(
+                        (variableDefinition.subjectFields ?? []).map((field) => ({
+                            key: field,
+                            title: SUBJECT_FIELD_LOOKUP[field] ?? field,
+                        })),
+                    )
+                }
+            />
+        </Card>
+    );
 };
 
-export { VardefSearchHit };
+export {VardefSearchHit};
