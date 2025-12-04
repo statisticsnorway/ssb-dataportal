3597e0a7c3ea1c2ce1668f58c358266f3acb1d09
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2025-12-04 09:13:33 +0100

    Generate client

---
+++ src/libs/data-access/variable-definitions/internal/models/index.ts
@@ -0,0 +1,21 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CompleteView';
+export * from './Contact';
+export * from './CreateDraft';
+export * from './CreatePatch';
+export * from './CreateValidityPeriod';
+export * from './CreateValidityPeriodContact';
+export * from './GetVardokVardefMappingById200Response';
+export * from './KlassReference';
+export * from './LanguageStringType';
+export * from './ListVariableDefinitions200ResponseInner';
+export * from './Owner';
+export * from './Problem';
+export * from './RenderedContact';
+export * from './RenderedView';
+export * from './SupportedLanguages';
+export * from './UpdateDraft';
+export * from './VardokIdResponse';
+export * from './VardokVardefIdPairResponse';
+export * from './VariableStatus';
