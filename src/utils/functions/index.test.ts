653a7385268d09770866c5e397f10f71fefd8b7c
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2025-12-05 14:42:39 +0100

    Add tests for fieldsNotNull

---
+++ src/utils/functions/index.test.ts
@@ -0,0 +1,24 @@
+'use client';
+import { fieldsNotNull } from '.';
+
+describe('fields not null filter', () => {
+  it('non-null objects pass through', () => {
+    expect([{ key1: 'key1' }, { key1: 'key2' }].filter(fieldsNotNull)).toStrictEqual([
+      { key1: 'key1' },
+      { key1: 'key2' },
+    ]);
+  });
+
+  it('objects with null fields filtered out', () => {
+    expect(
+      [
+        { key1: 'key1', key2: 'key2' },
+        { key1: null, key2: 'key2' },
+      ].filter(fieldsNotNull),
+    ).toStrictEqual([{ key1: 'key1', key2: 'key2' }]);
+  });
+
+  it('objects with undefined fields filtered out', () => {
+    expect([{ key1: 'key1' }, { key1: undefined }].filter(fieldsNotNull)).toStrictEqual([{ key1: 'key1' }]);
+  });
+});
