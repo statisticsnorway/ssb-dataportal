4bed59e1994e89391f7af226837631da8b95d63b
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-12-09 18:29:49 +0100

    rename tests

--- src/e2e/classifications.spec.ts
+++ src/e2e/classifications.spec.ts
@@ -1,5 +1,7 @@
 import { expect, test } from '@playwright/test';
 
+// This test should eventually test primarly what user can see and interact with on the
+// classifications overview page
 test.describe('Navigation classifications', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('/classifications');
