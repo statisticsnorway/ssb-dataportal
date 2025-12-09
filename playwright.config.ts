ed0153d62ec891bcb3de8db28072be41235e19d3
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-12-09 11:04:31 +0100

    redirect test

--- playwright.config.ts
+++ playwright.config.ts
@@ -73,7 +73,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'yarn dev',
+    command: 'NODE_ENV=test yarn dev',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
   },
