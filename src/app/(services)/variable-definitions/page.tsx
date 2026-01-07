8f808aad771d802b3c20e0be0cd42732c9059da5
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-01-07 15:55:45 +0100

    move data fetching to  vardef page

--- src/app/(services)/variable-definitions/page.tsx
+++ src/app/(services)/variable-definitions/page.tsx
@@ -1,36 +1,11 @@
-'use client';
-
-import { useMemo, useState } from 'react';
-import { FilterGroup } from '@/types/filters';
-import { useVardefTabData } from '@/utils/vardefTabContext';
+import { testVardefData } from '@/utils/mock-data';
 import VariableDefinitionsServicePage from './variable-definitions-service-page';
 
-export default function VariableDefinitions() {
-  const [selectedVariableDefinitions, setSelectedVariableDefinitions] = useState<string[]>([]);
-
-  const filterGroups: FilterGroup[] = useMemo(() => {
-    const groups: FilterGroup[] = [
-      {
-        filterHeading: 'Status',
-        filters: [
-          { label: 'Utkast', value: 'draft' },
-          { label: 'Publisert internt', value: 'published-internal' },
-          { label: 'Publisert eksternt', value: 'published-external' },
-        ],
-        selectedItems: selectedVariableDefinitions,
-        onFilterChange: setSelectedVariableDefinitions,
-      },
-    ];
-    return groups;
-  }, [selectedVariableDefinitions]);
-
-  const vardefData = useVardefTabData();
-
-  if (!vardefData) {
+export default async function VariableDefinitions() {
+  const { variableDefinitions } = testVardefData;
+  if (!variableDefinitions) {
     return <div>Loading...</div>;
   }
 
-  const { variableDefinitions } = vardefData;
-
-  return <VariableDefinitionsServicePage rawHits={variableDefinitions} filterGroups={filterGroups} isLoading={false} />;
+  return <VariableDefinitionsServicePage rawHits={variableDefinitions} isLoading={false} />;
 }
