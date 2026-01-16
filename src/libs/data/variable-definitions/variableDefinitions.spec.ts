55bbc501ea60140b6f4f7e6083b68a0cda2318d4
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2026-01-16 09:03:31 +0100

    Add structure to unit tests

--- src/libs/data/variable-definitions/variableDefinitions.spec.ts
+++ src/libs/data/variable-definitions/variableDefinitions.spec.ts
@@ -16,47 +16,63 @@ afterAll(() => {
 });
 
 describe('vardef data fetching', () => {
-  it('getVardefClient no token available', () => {
-    expect(getVardefClient()).rejects.toEqual('Could not retrieve access token!');
-  });
-  it('getVardefClient hardcoded token', () => {
-    process.env.METADATA_CATALOG_JWT_TOKEN = 'my-cool-token';
-    process.env.VARDEF_BASE_PATH = 'my-cool-base-path';
-    getVardefClient().then((client) => {
-      expect(client).toBeInstanceOf(VariableDefinitionsApi);
-      // @ts-ignore only protected access in test
-      const tokenReturn = client.configuration.accessToken?.();
-      if (tokenReturn instanceof Promise) {
-        tokenReturn.then((token: String) => {
-          expect(token).toEqual(process.env.METADATA_CATALOG_JWT_TOKEN);
+  describe('getVardefClient', () => {
+    it('no token available', () => {
+      expect(getVardefClient()).rejects.toEqual('Could not retrieve access token!');
+    });
+    it('hardcoded token', () => {
+      process.env.METADATA_CATALOG_JWT_TOKEN = 'my-cool-token';
+      process.env.VARDEF_BASE_PATH = 'my-cool-base-path';
+      getVardefClient().then((client) => {
+        expect(client).toBeInstanceOf(VariableDefinitionsApi);
+        // @ts-ignore only protected access in test
+        const tokenReturn = client.configuration.accessToken?.();
+        if (tokenReturn instanceof Promise) {
+          tokenReturn.then((token: String) => {
+            expect(token).toEqual(process.env.METADATA_CATALOG_JWT_TOKEN);
+          });
+        }
+        // @ts-ignore only protected access in test
+        expect(client.configuration.basePath).toEqual(process.env.VARDEF_BASE_PATH);
+      });
+    });
+  }),
+    describe('listRenderedVariableDefinitions', () => {
+      it('static data', () => {
+        expect(listRenderedVariableDefinitions()).resolves.toContainEqual(staticDefs[0]);
+      });
+      it('no token available', () => {
+        process.env.VARDEF_USE_STATIC_DATA = 'false';
+        expect(listRenderedVariableDefinitions()).rejects.toEqual('Could not retrieve access token!');
+      });
+      it('mock api call happy path', () => {
+        process.env.VARDEF_USE_STATIC_DATA = 'false';
+        process.env.METADATA_CATALOG_JWT_TOKEN = 'my-cool-token';
+
+        jest.spyOn(VariableDefinitionsApi.prototype, 'listVariableDefinitions').mockResolvedValue(staticDefs);
+
+        listRenderedVariableDefinitions().then((result) => {
+          expect(result).toContainEqual(staticDefs[2]);
         });
-      }
-      // @ts-ignore only protected access in test
-      expect(client.configuration.basePath).toEqual(process.env.VARDEF_BASE_PATH);
-    });
-  });
-  it('listRenderedVariableDefinitions static data', () => {
-    expect(listRenderedVariableDefinitions()).resolves.toContainEqual(staticDefs[0]);
-  });
-  it('listRenderedVariableDefinitions no token available', () => {
-    process.env.VARDEF_USE_STATIC_DATA = 'false';
-    expect(listRenderedVariableDefinitions()).rejects.toEqual('Could not retrieve access token!');
-  });
-  it('listRenderedVariableDefinitions', () => {
-    process.env.VARDEF_USE_STATIC_DATA = 'false';
-    process.env.METADATA_CATALOG_JWT_TOKEN = 'my-cool-token';
+      });
+    }),
+    describe('getRenderedVariableDefinition', () => {
+      it('static data', () => {
+        expect(getRenderedVariableDefinition('Xd3Ueog_')).resolves.toEqual(staticDefs[5]);
+      });
+      it('no token available', () => {
+        process.env.VARDEF_USE_STATIC_DATA = 'false';
+        expect(getRenderedVariableDefinition('Xd3Ueog_')).rejects.toEqual('Could not retrieve access token!');
+      });
+      it('mock api call happy path', () => {
+        process.env.VARDEF_USE_STATIC_DATA = 'false';
+        process.env.METADATA_CATALOG_JWT_TOKEN = 'my-cool-token';
 
-    jest.spyOn(VariableDefinitionsApi.prototype, 'listVariableDefinitions').mockResolvedValue(staticDefs);
+        jest.spyOn(VariableDefinitionsApi.prototype, 'getVariableDefinitionById').mockResolvedValue(staticDefs[5]);
 
-    listRenderedVariableDefinitions().then((result) => {
-      expect(result).toContainEqual(staticDefs[2]);
+        getRenderedVariableDefinition('Xd3Ueog_').then((result) => {
+          expect(result).toEqual(staticDefs[5]);
+        });
+      });
     });
-  });
-  it('listRenderedVariableDefinitions static data', () => {
-    expect(getRenderedVariableDefinition('Xd3Ueog_')).resolves.toEqual(staticDefs[5]);
-  });
-  it('getRenderedVariableDefinition no token available', () => {
-    process.env.VARDEF_USE_STATIC_DATA = 'false';
-    expect(getRenderedVariableDefinition('Xd3Ueog_')).rejects.toEqual('Could not retrieve access token!');
-  });
 });
