e850393a1104f8ec5971c064132aa92c8e2b8d5a
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2026-01-16 08:50:08 +0100

    Fix typing for async parameters

--- src/app/(services)/variable-definitions/[id]/page.tsx
+++ src/app/(services)/variable-definitions/[id]/page.tsx
@@ -2,14 +2,13 @@ import { getRenderedVariableDefinition } from '@/libs/data/variable-definitions/
 import { RenderedView } from '@/libs/data-access/variable-definitions/internal/models';
 import VariableDefinitionDetail from './variableDefinitionDetail';
 
-export default async function VariableDefinition({ params }: { params: { id: string } }) {
-  // await is necessary for Next
+export default async function VariableDefinition({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
   let variableDefinition: RenderedView | undefined = undefined;
   try {
     variableDefinition = await getRenderedVariableDefinition(id);
-  } catch {
-    console.error('Could not fetch variable definition');
+  } catch (error: unknown) {
+    console.error('Could not fetch variable definition', error);
   }
 
   return <VariableDefinitionDetail variableDefinition={variableDefinition} />;
