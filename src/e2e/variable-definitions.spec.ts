0454720d02c0f434330c0fda85ec5b27856c67aa
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-01-19 22:23:40 +0100

    add test

--- src/e2e/variable-definitions.spec.ts
+++ src/e2e/variable-definitions.spec.ts
@@ -8,3 +8,17 @@ test.describe('Navigation variable definitions', () => {
     await expect(page).toHaveURL(/\/variable-definitions$/);
   });
 });
+
+// This test depends on current testdata
+test('test', async ({ page }) => {
+  await page.goto('/variable-definitions');
+  await page.getByRole('checkbox', { name: 'Arbeid og lønn' }).check();
+  await page.getByRole('checkbox', { name: 'Inntekt og forbruk' }).check();
+  await page.getByRole('checkbox', { name: 'Sosiale forhold og' }).check();
+  await page
+    .getByRole('listitem')
+    .filter({ hasText: 'Sosiale forhold og kriminalitet (2)x' })
+    .getByRole('button')
+    .click();
+  await page.getByRole('listitem').filter({ hasText: 'Fjern alle filterex' }).getByRole('button').click();
+});
