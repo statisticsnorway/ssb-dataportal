ff32fcd5eb155678f4b76c647aaa6c9551c61a25
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2026-01-19 10:58:07 +0100

    Remove unused constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,12 +1,9 @@
-export const VARDEF_PUBLIC_HOST = 'https://vardef.intern.test.ssb.no/public/variable-definitions';
 export const ID = 'ID';
 export const NB = 'nb';
 export const ACCEPT_LANGUAGE = 'accept-language';
 
-export const KLASS_HOST = 'https://data.ssb.no/api/klass/v1/';
 export const CLASSIFICATIONS = 'classifications';
 export const CLASSIFICATION_FAMILIES = 'classificationfamilies';
-export const API_VARIABLE_DEFINITIONS = 'api/variable-definitions';
 
 export const KLASSIFIKASJONER = 'Klassifikasjoner';
 export const DATASETS = 'Datasett';
