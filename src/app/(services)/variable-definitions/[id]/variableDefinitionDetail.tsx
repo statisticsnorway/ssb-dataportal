35bc5b291c6afd7548b1165e51fedf1671a212e2
Jorgen-5 <rlj@ssb.no>
2026-01-27 15:51:50 +0100

    Merge

--- src/app/(services)/variable-definitions/[id]/variableDefinitionDetail.tsx
+++ src/app/(services)/variable-definitions/[id]/variableDefinitionDetail.tsx
@@ -2,11 +2,6 @@
 
 import { Button, Tag } from '@digdir/designsystemet-react';
 import { ClipboardCheckmarkIcon, ClipboardIcon } from '@navikt/aksel-icons';
-
-
-import { PYPI_DAPLA_TOOLBELT_METADATA_URL } from './components/constants';
-
-
 import { Breadcrumbs, BreadcrumbType } from '@/components/breadcrumbs';
 import { DetailsPagePanel } from '@/components/details-page-panel/details-page-panel';
 import { TextField } from '@/components/text-field';
@@ -15,7 +10,7 @@ import { RenderedView } from '@/libs/data-access/variable-definitions/internal';
 import { localization } from '@/libs/language';
 import { convertStatus, nonEmpty } from '@/utils/functions';
 import { CodeSnippet } from './components/codeSnippet';
-import { COPIED_LABEL, COPY_ID_LABEL } from './components/constants';
+import { COPIED_LABEL, COPY_ID_LABEL, PYPI_DAPLA_TOOLBELT_METADATA_URL } from './components/constants';
 import { VardefHeading } from './components/vardefHeading';
 
 import {
