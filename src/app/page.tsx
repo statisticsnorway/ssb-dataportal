09b892e834df1551ae3b6a3395af85b7aee87c90
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-01-28 13:32:49 +0100

    add navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import { Heading } from '@digdir/designsystemet-react';
+import Link from 'next/link';
+import { localization } from '@/libs/language';
+import { tabs } from '@/utils/constants';
+
 export default function Home() {
   return (
     <div className='pageContainer container'>
-      <p>Landingpage</p>
+      <header>
+        <Heading level={2}>{localization.info.landingPageTitle}</Heading>
+        <section>{localization.info.landingPageInfo}</section>
+      </header>
+      <main>
+        {tabs.map((item, index) => (
+          <Link key={index} href={item.href}>
+            {item.label}
+          </Link>
+        ))}
+      </main>
     </div>
   );
 }
