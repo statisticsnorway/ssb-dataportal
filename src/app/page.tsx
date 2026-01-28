7d093b0c6ce15249b91134e32b3223958cd670de
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-01-28 11:18:15 +0100

    add landingpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
-import { redirect } from 'next/navigation';
-
 export default function Home() {
-  redirect('/variable-definitions');
+  return (
+    <div className='pageContainer container'>
+      <p>Landingpage</p>
+    </div>
+  );
 }
