4a83217e693de44b1d86e49ff381a5a01e80a5a2
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-01-28 14:48:49 +0100

    add navigation test

---
+++ src/e2e/dataportal.spec.ts
@@ -0,0 +1,20 @@
+import { expect, test } from '@playwright/test';
+import { localization } from '@/libs/language';
+
+test.describe('Navigation variable definitions', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+    await expect(page).toHaveURL('/');
+  });
+});
+
+test('Navigate from landingpage', async ({ page }) => {
+  await page.goto('/');
+
+  await expect(page.getByRole('heading')).toContainText(localization.info.landingPageTitle);
+  await page.getByRole('link', { name: localization.variableDefinitions }).click();
+  await expect(page.getByRole('tab', { name: localization.variableDefinitions })).toBeVisible();
+  await expect(page.locator('#tab-_r_3_')).toContainText(localization.variableDefinitions);
+  await page.getByRole('link', { name: 'Statistics Norway logo' }).click();
+  await expect(page.getByRole('heading')).toContainText(localization.info.landingPageTitle);
+});
