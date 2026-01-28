663913b27eb9617ab6073fff09d8a0544c65af02
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-01-28 23:11:54 +0100

    simple homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,36 @@
-import { Card, Heading } from '@digdir/designsystemet-react';
+import { Heading, Paragraph } from '@digdir/designsystemet-react';
 import Link from 'next/link';
 import { localization } from '@/libs/language';
 import { tabs } from '@/utils/constants';
+import styles from './home.module.css';
 
 export default function Home() {
   return (
-    <div id='pageContainer' className='container'>
-      <header>
-        <Heading level={2}>{localization.info.landingPageTitle}</Heading>
-        <section>{localization.info.landingPageInfo}</section>
-      </header>
-      <main id='landingPageContent'>
-        {tabs.map((item, index) => (
-          <Card key={index} className='linkCard'>
+    <div className='container'>
+      <div className={styles.pageContainer}>
+        <header>
+          <Heading level={2} data-size='xl' className={styles.title}>
+            {localization.info.landingPageTitle}
+          </Heading>
+          <Paragraph data-size='md' className={styles.paragraph}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus risus elit, rhoncus id rutrum at, aliquam
+            et lacus. Nam non ipsum augue. Vestibulum lorem mi, venenatis tristique commodo ac, auctor ac purus. Nunc et
+            eros id magna suscipit commodo. Sed imperdiet, lacus et sodales cursus, dui augue tempor sem, viverra
+            bibendum leo mi ac dui. Vivamus eget finibus lorem, sit amet bibendum arcu. Morbi porttitor convallis
+            semper. Quisque venenatis congue quam, ac pretium justo eleifend eu. Proin aliquam pellentesque leo eu
+            semper. Sed vitae mi sit amet enim commodo blandit ut a ligula. Ut malesuada orci arcu, id accumsan felis
+            commodo nec. Mauris eu ipsum nec arcu placerat dictum. Aliquam eget fringilla erat, eu lacinia magna.
+            Interdum et malesuada fames ac ante ipsum primis in faucibus.
+          </Paragraph>
+        </header>
+        <main className={styles.landingPageContent}>
+          {tabs.map((item, index) => (
             <Link key={index} href={item.href}>
               {item.label}
             </Link>
-          </Card>
-        ))}
-      </main>
+          ))}
+        </main>
+      </div>
     </div>
   );
 }
