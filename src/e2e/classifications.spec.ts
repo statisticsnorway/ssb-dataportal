0976e03f87077b986bbf43990665ece218596216
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-01-29 12:42:50 +0100

    add test and remove before each - not useful

--- src/e2e/classifications.spec.ts
+++ src/e2e/classifications.spec.ts
@@ -1,11 +1,7 @@
 import { expect, test } from '@playwright/test';
-import { classificationsPath } from '@/utils/constants';
+import { tabsData } from '@/app/(services)/tabs';
 
-// This test should eventually test primarly what user can see and interact with on the
-// classifications overview page
-test.describe('Navigation classifications', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto(classificationsPath);
-    await expect(page).toHaveURL(classificationsPath);
-  });
+test('Classifications page', async ({ page }) => {
+  await page.goto(tabsData.Classifications.route);
+  await expect(page).toHaveURL(tabsData.Classifications.route);
 });
