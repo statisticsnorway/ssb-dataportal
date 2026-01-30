1facfd58a3f4382b053b5f2ccbc9bf6313f30314
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-01-30 20:46:32 +0100

    add background image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Alert, Card, Heading, Paragraph } from '@digdir/designsystemet-react';
+import { Alert, Card, Divider, Heading, Paragraph } from '@digdir/designsystemet-react';
 import Link from 'next/link';
 import ClientOnly from '@/components/client-details/clientOnly';
 import { localization } from '@/libs/language';
@@ -25,24 +25,28 @@ export default function Home() {
       <Alert data-color={'warning'} className='infoAlert' data-size={'md'}>
         Dataportalens forside er under arbeid.
       </Alert>
-      <div className='container'>
-        <main className={styles.pageContainer}>
-          <header className={styles.header}>
-            <Heading level={1} data-size='xl' className={styles.title}>
-              {localization.info.landingPageTitle}
-            </Heading>
-            <div className={styles.backgroundSubTitle}>
-              <Paragraph data-size='xl' className={styles.subTitle}>
-                {localization.info.landingPageSubTitle}
-              </Paragraph>
-            </div>
-
-            {detailsList.map((detail, index) => (
-              <Card key={index} style={{ marginBottom: '1rem' }} data-color='brand1'>
-                <ClientOnly summary={detail.summary} content={detail.content} />
-              </Card>
-            ))}
-          </header>
+      <div className={`${styles.background} rootContainer`}>
+        <main className={`${styles.pageContainer} container`}>
+          <Card data-color='brand1' variant='tinted' className={styles.headerCard}>
+            <header className={styles.header}>
+              <Heading level={1} data-size='xl' className={styles.title}>
+                {localization.info.landingPageTitle}
+              </Heading>
+              <div className={styles.backgroundSubTitle}>
+                <Paragraph data-size='xl' className={styles.subTitle}>
+                  {localization.info.landingPageSubTitle}
+                </Paragraph>
+              </div>
+              <div className={styles.infoDetails}>
+                {detailsList.map((detail, index) => (
+                  <Card key={index} className={styles.infoCards}>
+                    <ClientOnly summary={detail.summary} content={detail.content} />
+                  </Card>
+                ))}
+              </div>
+            </header>
+          </Card>
+          <Divider className={styles.customDivider} />
           <nav className={styles.landingPageContent}>
             {Object.values(tabsData).map((item, index) => (
               <Link key={index} href={item.route}>
