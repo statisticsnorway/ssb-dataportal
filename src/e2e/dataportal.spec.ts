d7cf489a1edb79cea2d447f86e0bfd70c0a9d778
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-01-31 03:10:16 +0100

    rename file

--- src/e2e/dataportal.spec.ts
+++ src/e2e/dataportal.spec.ts
@@ -6,14 +6,14 @@ test.describe('Navigation variable definitions', () => {
     await page.goto('/');
     await expect(page).toHaveURL('/');
   });
-});
 
-test('Navigate from landingpage', async ({ page }) => {
-  await page.goto('/');
-  await expect(page.getByRole('heading')).toContainText(localization.info.landingPageTitle);
-  await page.getByRole('link', { name: localization.tabs.variableDefinitions }).click();
-  await expect(page.getByRole('tab', { name: localization.tabs.variableDefinitions })).toBeVisible();
-  await expect(page.locator('#tab-_r_3_')).toContainText(localization.tabs.variableDefinitions);
-  await page.getByRole('link', { name: localization.statisticsNorway }).click();
-  await expect(page.getByRole('heading')).toContainText(localization.info.landingPageTitle);
+  test('Navigate from landingpage', async ({ page }) => {
+    await page.goto('/');
+    await expect(page.getByRole('heading')).toContainText(localization.info.landingPageTitle);
+    await page.getByRole('link', { name: localization.tabs.variableDefinitions }).click();
+    await expect(page.getByRole('tab', { name: localization.tabs.variableDefinitions })).toBeVisible();
+    await expect(page.locator('#tab-_r_3_')).toContainText(localization.tabs.variableDefinitions);
+    await page.getByRole('link', { name: localization.statisticsNorway }).click();
+    await expect(page.getByRole('heading')).toContainText(localization.info.landingPageTitle);
+  });
 });
