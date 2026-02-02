194f0ea6c93dc7c0be45c93af2cc0115f7e02686
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-02-02 20:16:39 +0100

    viewport

--- src/e2e/dataportal.spec.ts
+++ src/e2e/dataportal.spec.ts
@@ -38,4 +38,40 @@ test.describe('Landing page', () => {
     await page.getByRole('link', { name: localization.statisticsNorway }).click();
     await expect(page.getByRole('heading', { level: 1 })).toContainText(localization.info.landingPageTitle);
   });
+
+  test('test with dynamic viewport resize', async ({ page }) => {
+    await page.setViewportSize({ width: 400, height: 600 });
+    await expect(page.getByRole('link', { name: 'Statistisk sentralbyrå logo' })).toBeVisible();
+    await expect(page.getByRole('banner')).toContainText('Dataportal');
+    await expect(page.getByRole('heading', { level: 1 })).toContainText(localization.info.landingPageTitle);
+    await page.getByRole('link', { name: localization.tabs.variableDefinitions }).click();
+    await expect(page.getByRole('tab', { name: localization.tabs.variableDefinitions })).toBeVisible();
+    await expect(page.getByRole('tab', { name: localization.tabs.variableDefinitions })).toContainText(
+      localization.tabs.variableDefinitions,
+    );
+    await page.getByRole('link', { name: localization.statisticsNorway }).click();
+    await expect(page.getByRole('heading', { level: 1 })).toContainText(localization.info.landingPageTitle);
+  });
+
+  test('Header test with dynamic viewport resize', async ({ page }) => {
+    await page.setViewportSize({ width: 320, height: 600 });
+    await expect(page.getByRole('link', { name: 'Statistisk sentralbyrå logo' })).toBeVisible();
+    await expect(page.getByRole('banner')).toContainText('Dataportal');
+
+    await page.setViewportSize({ width: 800, height: 600 });
+    await expect(page.getByRole('link', { name: 'Statistisk sentralbyrå logo' })).toBeVisible();
+    await expect(page.getByRole('banner')).toContainText('Dataportal');
+    await page.setViewportSize({ width: 1280, height: 1024 });
+  });
+});
+
+test.describe('specific viewport block', () => {
+  test.use({ viewport: { width: 320, height: 600 } });
+
+  test('my test', async ({ page }) => {
+    await page.goto('/');
+    await expect(page.getByRole('link', { name: 'Statistisk sentralbyrå logo' })).toBeVisible();
+    await expect(page.getByRole('banner')).toContainText('Dataportal');
+    await expect(page.getByRole('banner')).toBeInViewport();
+  });
 });
