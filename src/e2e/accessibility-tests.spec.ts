fa511b716aa021d8a84795a5227f243660a82269
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-02-04 10:39:52 +0100

    remove duplicate test

--- src/e2e/accessibility-tests.spec.ts
+++ src/e2e/accessibility-tests.spec.ts
@@ -106,11 +106,4 @@ test.describe('Landingpage – accessibility', () => {
 
     expect(results.violations).toEqual([]);
   });
-
-  test('Landingpage navigation', async ({ page }) => {
-    await page.goto('/');
-    const results = await new AxeBuilder({ page }).include('nav#menu').exclude('.ds-alert.infoAlert').analyze();
-
-    expect(results.violations).toEqual([]);
-  });
 });
