bd7d823bacd020efeb9e8ff1242356b21bc4eb9f
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-02-05 14:35:49 +0100

    add component to ensure all external links are handled correctly

---
+++ src/components/external-link/index.tsx
@@ -0,0 +1,24 @@
+import { Link } from '@digdir/designsystemet-react';
+
+interface ExternalLinkProps {
+  linkText: string;
+  href: string;
+}
+
+/**
+ * Renders a Designsystem `Link` that opens an external URL in a safe way.
+ * Adds `rel="noreferrer"` to prevent passing referrer information.
+ *
+ * @param href - The URL to navigate to.
+ * @param linkText - Text to display for the link.
+ * @returns A `Link` component rendering the given text and URL.
+ */
+const ExternalLink = ({ linkText, href }: ExternalLinkProps) => {
+  return (
+    <Link rel='noreferrer' href={href}>
+      {linkText}
+    </Link>
+  );
+};
+
+export { ExternalLink };
