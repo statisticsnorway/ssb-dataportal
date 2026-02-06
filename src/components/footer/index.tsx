98f05ebe71500c2244b5d31493f0d086e08d8e94
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-02-06 15:11:23 +0100

    simplify footer and align email with form

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,6 +1,8 @@
-import { Alert, Heading, Link, Paragraph } from '@digdir/designsystemet-react';
+import { Card, Link, Paragraph } from '@digdir/designsystemet-react';
+import { EnvelopeOpenIcon } from '@navikt/aksel-icons';
 import { FC } from 'react';
 import { ExternalLink } from '@/components/external-link';
+import PencilIcon from '@/icons/pencil.svg';
 import { localization } from '@/libs/language';
 import { UrlItem } from '@/types/navigationTypes';
 import styles from './footer.module.css';
@@ -11,25 +13,24 @@ export interface FooterProps {
   footerLinks?: UrlItem[];
 }
 
-export const Footer: FC<FooterProps> = ({ footerLinks }) => (
+export const Footer: FC<FooterProps> = () => (
   <footer className={styles.footer}>
-    <Alert data-color='info' className={styles.infoSection}>
-      <Heading level={3}>Tilbakemeldinger</Heading>
-      <Paragraph>
-        Dataportalen er under utvikling, og dine innspill er viktige for oss. Hjelp oss gjerne ved å fylle ut{' '}
-        <ExternalLink
-          willOpenNewTab={true}
-          linkText={localization.feedBackForm}
-          href='https://forms.office.com/Pages/ResponsePage.aspx?id=knAhx0CyHU69YfqXupdcvG8mQNraR5ZAu3es4-se84xUN0VFME5BSVFSUTZDRUZCTzNTVUlFTDlUNC4u'
-        />
-      </Paragraph>
-    </Alert>
-    <section className={styles.footerLinkSection}>
-      {footerLinks?.map((link, index) => (
-        <Link key={index} href={link.url}>
-          {link.name}
-        </Link>
-      ))}
-    </section>
+    <div className={`${styles.footerWrapper} container`}>
+      <Card className={styles.infoSection}>
+        <PencilIcon width={'60px'} />
+        <Paragraph>
+          Dataportalen er under utvikling, og dine innspill er viktige for oss. Hjelp oss gjerne ved å fylle ut{' '}
+          <ExternalLink
+            willOpenNewTab={true}
+            linkText={localization.feedBackForm}
+            href='https://forms.office.com/Pages/ResponsePage.aspx?id=knAhx0CyHU69YfqXupdcvG8mQNraR5ZAu3es4-se84xUN0VFME5BSVFSUTZDRUZCTzNTVUlFTDlUNC4u'
+          />
+        </Paragraph>
+      </Card>
+      <section className={styles.footerLinkSection}>
+        <EnvelopeOpenIcon title={localization.contact.label} fontSize='2rem' />
+        <Link href='mailto:metadata@ssb.no'>metadata@ssb.no</Link>
+      </section>
+    </div>
   </footer>
 );
