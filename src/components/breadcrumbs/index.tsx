b344fbc07983fda1c8854c32264a7a081f68ae55
Jan <63044278+JanhSander@users.noreply.github.com>
2026-02-10 09:51:49 +0100

    Removed redundant fragment

--- src/components/breadcrumbs/index.tsx
+++ src/components/breadcrumbs/index.tsx
@@ -15,28 +15,26 @@ export interface BreadcrumbsProps {
 
 export const Breadcrumbs = ({ breadcrumbList, homeUrl, breadcrumbHomeAriaLabel }: BreadcrumbsProps) => {
   return (
-    <>
-      <nav className={styles.breadcrumbs}>
-        <span>
-          <Link className={styles.link} aria-label={breadcrumbHomeAriaLabel} href={homeUrl.href}>
-            {homeUrl.text}
-          </Link>
-          {breadcrumbList?.map((breadcrumb, i) => {
-            return (
-              <span key={hashCode(breadcrumb.href)}>
-                <span className={styles.separator}>{'>'}</span>
-                {i === breadcrumbList.length - 1 ? (
-                  <span className={styles.deactiveLink}>{breadcrumb.text}</span>
-                ) : (
-                  <Link href={breadcrumb.href} className={styles.link}>
-                    {breadcrumb.text}
-                  </Link>
-                )}
-              </span>
-            );
-          })}
-        </span>
-      </nav>
-    </>
+    <nav className={styles.breadcrumbs}>
+      <span>
+        <Link className={styles.link} aria-label={breadcrumbHomeAriaLabel} href={homeUrl.href}>
+          {homeUrl.text}
+        </Link>
+        {breadcrumbList?.map((breadcrumb, i) => {
+          return (
+            <span key={hashCode(breadcrumb.href)}>
+              <span className={styles.separator}>{'>'}</span>
+              {i === breadcrumbList.length - 1 ? (
+                <span className={styles.deactiveLink}>{breadcrumb.text}</span>
+              ) : (
+                <Link href={breadcrumb.href} className={styles.link}>
+                  {breadcrumb.text}
+                </Link>
+              )}
+            </span>
+          );
+        })}
+      </span>
+    </nav>
   );
 };
