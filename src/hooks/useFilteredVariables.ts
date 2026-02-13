f8b60cce577ad62af6e946941b1af828282ddf6d
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-02-13 11:35:51 +0100

    add status

--- src/hooks/useFilteredVariables.ts
+++ src/hooks/useFilteredVariables.ts
@@ -8,6 +8,7 @@ interface UseFilteredVariablesProps {
   variablesPromise: Promise<{ data: RenderedView[]; error: Error | null }>;
   textFilter: string;
   subjectFilters: FilterItem[];
+  statusFilters: FilterItem[];
   sortOption: SortTypes;
 }
 
@@ -15,12 +16,13 @@ export function useFilteredVariables({
   variablesPromise,
   textFilter,
   subjectFilters,
+  statusFilters,
   sortOption,
 }: UseFilteredVariablesProps) {
   const { data: variables, error } = use(variablesPromise);
 
   const filteredVariables = useMemo(
-    () => filterAndSortVariables(variables, textFilter, subjectFilters, sortOption),
+    () => filterAndSortVariables(variables, textFilter, subjectFilters, statusFilters, sortOption),
     [variables, textFilter, subjectFilters, sortOption],
   );
 
