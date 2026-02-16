41266adb85d46d3723b2cc5346598d1bc674fecb
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-02-16 13:45:02 +0100

    Remove dependency with transitive vulnerability (#141)
    * remove dependency with transitive vulnerability
    
    * do not import webpack
    
    * remove webpack config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from 'next';
-import type { RuleSetRule } from 'webpack';
 import path from 'path';
 
 const isTest = process.env.NODE_ENV === 'test';
@@ -18,37 +17,6 @@ const nextConfig: NextConfig = {
         as: '*.js',
       },
     },
-  },
-  /* config options for Webpack here */
-  webpack(config: { module: { rules: any[] }; resolve: { alias: any } }) {
-    const fileLoaderRule = config.module.rules.find(
-      (rule: RuleSetRule) => !!rule.test && rule.test instanceof RegExp && rule.test.test('.svg'),
-    );
-
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/, // *.svg?url
-      },
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
-        use: ['@svgr/webpack'],
-      },
-    );
-
-    // Modify the file loader rule to ignore *.svg, since we have it handled now.
-    fileLoaderRule.exclude = /\.svg$/i;
-
-    // Aliases
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      '@global-css': path.resolve(__dirname, 'src/app/global.css'),
-    };
-
-    return config;
   },
   typescript: {
     ignoreBuildErrors: true,
