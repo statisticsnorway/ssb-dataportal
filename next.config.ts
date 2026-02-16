a3b0c44817131df3d1cf4d495be80878516fe124
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-02-16 13:29:34 +0100

    remove webpack config

--- next.config.ts
+++ next.config.ts
@@ -18,35 +18,6 @@ const nextConfig: NextConfig = {
       },
     },
   },
-  /* config options for Webpack here */
-  webpack(config: { module: { rules: any[] }; resolve: { alias: any } }) {
-    const fileLoaderRule = config.module.rules.find(
-      (rule: any) => !!rule.test && rule.test instanceof RegExp && rule.test.test('.svg'),
-    );
-
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/, // *.svg?url
-      },
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
-        use: ['@svgr/webpack'],
-      },
-    );
-
-    fileLoaderRule.exclude = /\.svg$/i;
-
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      '@global-css': path.resolve(__dirname, 'src/app/global.css'),
-    };
-
-    return config;
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
