d5b90ddb66a4d10b869b255873745847c5921cfd
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-03-10 14:53:55 +0100

    add set up for jest dom css supports

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,10 @@
 import '@testing-library/jest-dom';
+
+if (!window.CSS) {
+  // @ts-expect-error jsdom polyfill
+  window.CSS = {};
+}
+
+if (!window.CSS.supports) {
+  window.CSS.supports = () => false;
+}
