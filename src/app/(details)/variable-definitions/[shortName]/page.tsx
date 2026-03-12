9c98204855702788df2ea3f5cac1db38819aa1a2
Jorgen-5 <jrussnes@gmail.com>
2026-03-12 14:32:05 +0100

    Add more logs (#203)
    * Remove client logs that should not be exposed
    
    * Json logging
    
    * Update loggin
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Add log level to manifests
    
    * Fix failing test
    
    * Fix failing test
    
    * New logs
    
    ---------
    
    Co-authored-by: Jorgen-5 <rlj@ssb.no>

--- src/app/(details)/variable-definitions/[shortName]/page.tsx
+++ src/app/(details)/variable-definitions/[shortName]/page.tsx
@@ -1,11 +1,21 @@
 import { notFound } from 'next/navigation';
 import { getRenderedVariableDefinition } from '@/libs/data/variable-definitions/variableDefinitions';
 import { RenderedView } from '@/libs/data-access/variable-definitions/internal/models';
+import { sanitizeError } from '@/libs/logger/sanitize';
+import { createLogger } from '@/libs/logger/server-logger';
 import VariableDefinitionDetail from './variableDefinitionDetail';
+
+const logger = createLogger('variable-definitions:detail');
 
 export default async function VariableDefinition({ params }: { params: Promise<{ shortName: string }> }) {
   const { shortName } = await params;
-  const variableDefinition: RenderedView = await getRenderedVariableDefinition(shortName).catch(() => notFound());
+  logger.info({ shortName }, 'Variable definition detail page access');
+
+  const variableDefinition: RenderedView = await getRenderedVariableDefinition(shortName).catch((error) => {
+    logger.error({ shortName, error: sanitizeError(error) }, 'Failed to load variable definition details');
+    notFound();
+  });
+
   const daplaLabVardefUrl: string | undefined = process.env.DAPLA_LAB_VARDEF_URL;
   return <VariableDefinitionDetail variableDefinition={variableDefinition} daplaLabVardefUrl={daplaLabVardefUrl} />;
 }
