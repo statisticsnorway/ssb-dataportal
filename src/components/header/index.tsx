3362766fa5c4be6630b0c27f06aa475772c18cbf
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-03-16 17:01:02 +0100

    do - use

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -14,7 +14,7 @@ export const Header: FC<HeaderProps> = ({ homeUrl, title, devEnvironmentName }) 
   return (
     <header className={styles.header} role='banner'>
       <div className={`${styles.headerContainer} container`}>
-        <Link href={homeUrl} aria-label={localization.navigateHome} className={styles.logoAndTitle}>
+        <Link href={homeUrl} title={localization.navigateHome} className={styles.logoAndTitle}>
           <Image
             className={styles.logo}
             src='/ssb-logo.svg'
