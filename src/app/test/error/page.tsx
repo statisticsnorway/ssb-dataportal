4be43375a886926377ffc0dfdce56af4da330f76
Jan <63044278+JanhSander@users.noreply.github.com>
2026-03-16 20:12:54 +0100

    Add comment

--- src/app/test/error/page.tsx
+++ src/app/test/error/page.tsx
@@ -1,4 +1,5 @@
 export default function Page() {
+  // Avoid breaking the production build when Next.js prerenders pages, but still throw an error in development to test the error page.
   if (process.env.NODE_ENV !== 'production') {
     throw new Error('Testing error page');
   }
