ac594c40b5f882943e2d058072903470be042fa4
Jorgen-5 <rlj@ssb.no>
2026-04-08 14:05:47 +0200

    Comment

--- src/app/authContext.tsx
+++ src/app/authContext.tsx
@@ -22,6 +22,9 @@ interface AuthProviderProps {
 }
 
 export const AuthProvider = (props: AuthProviderProps) => {
+  // TODO: Remove or change when authentication is more complete
+  // Currently always setting the value to true when the app is running on NAIS, if not one can toggle it by using NEXT_PUBLIC_AUTH_OVERRIDE
+  // NEXT_PUBLIC_AUTH_OVERRIDE is currently used in the auth playwright tests to enure that we can test the fields that will not be visible to external users.
   const isAuthenticated =
     process.env.NODE_ENV === 'production' ? true : process.env.NEXT_PUBLIC_AUTH_OVERRIDE !== 'false';
   return <AuthContext.Provider value={{ isAuthenticated }}>{props.children}</AuthContext.Provider>;
