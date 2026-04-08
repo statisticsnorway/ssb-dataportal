881a133b2ac23bec91017202480b316291badba5
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2026-04-08 14:18:19 +0200

    Throw error if auth is disabled in deployed app

---
+++ src/libs/auth/userAuth.spec.ts
@@ -0,0 +1,33 @@
+import { afterAll, beforeEach, describe, expect, it, vi } from 'vitest';
+import { authenticateUser } from './userAuth';
+
+vi.mock('server-only', () => ({}));
+
+const ORIGINAL_ENV = process.env;
+beforeEach(() => {
+  // Allow independently modifying process.env in tests
+  // by resetting it before each
+  vi.resetModules();
+  process.env = { ...ORIGINAL_ENV };
+});
+afterAll(() => {
+  process.env = ORIGINAL_ENV; // Restore old environment
+});
+
+vi.mock('next/headers', () => ({
+  headers: vi.fn().mockResolvedValue({ get: () => `Bearer ${JWT}` }),
+}));
+
+describe('userAuth functions', async () => {
+  it('authenticateUser auth disabled', async () => {
+    process.env.DANGEROUSLY_DISABLE_USER_AUTH = 'true'
+    process.env.IS_AUTHENTICATED = 'true'
+    expect(authenticateUser()).toEqual({isAuthenticated: true});
+  });
+  it('authenticateUser auth disabled and app deployed', async () => {
+    process.env.DANGEROUSLY_DISABLE_USER_AUTH = 'true'
+    process.env.NAIS_CLUSTER_NAME = 'prod'
+    process.env.IS_AUTHENTICATED = 'true'
+    expect(authenticateUser).toThrow('User auth is disabled in deployed in environment!');
+  });
+});
--- src/libs/auth/userAuth.ts
+++ src/libs/auth/userAuth.ts
@@ -6,6 +6,7 @@ const logger = createLogger('user-auth');
 
 export function authenticateUser(): Auth {
   if (process.env.DANGEROUSLY_DISABLE_USER_AUTH === 'true') {
+    if (process.env.NAIS_CLUSTER_NAME != undefined) throw Error("User auth is disabled in deployed in environment!")
     logger.warn("Danger! User auth is disabled. This may allow unauthenticated users access. This message should only be visible in local dev environments.")
     return {isAuthenticated: process.env.IS_AUTHENTICATED === 'true'}
   }
