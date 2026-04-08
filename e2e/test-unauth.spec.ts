33bb0a07b5dbe43e3402f439590bc9206129d256
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-04-08 21:41:05 +0200

    simplify test skip

--- e2e/test-unauth.spec.ts
+++ e2e/test-unauth.spec.ts
@@ -2,7 +2,6 @@ import { localization } from '@/libs/language';
 import test, { expect } from '@playwright/test';
 
 test('Unauthenticated page test', async ({ page }, testInfo) => {
-  //testInfo.annotations.push({ type: 'allowUnauthChrome' });
   if (testInfo.project.name !== 'chrome-unauth') {
     testInfo.skip();
   }
