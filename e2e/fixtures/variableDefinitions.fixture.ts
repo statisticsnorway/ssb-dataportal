5eb67aafd9cf2fece1bdd30df8073b1b435ad148
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-04-08 21:15:12 +0200

    do not run unauth

--- e2e/fixtures/variableDefinitions.fixture.ts
+++ e2e/fixtures/variableDefinitions.fixture.ts
@@ -1,11 +1,17 @@
-import { test as base, expect, Page } from '@playwright/test';
+import { test as base, expect, Page, TestInfo } from '@playwright/test';
 import { tabsData } from '@/app/(services)/tabs';
 import { stabilize } from '../utils/commonUtils';
 
 export const test = base.extend<{
   variableDefinitionsPage: Page;
 }>({
-  variableDefinitionsPage: async ({ page }, use) => {
+  variableDefinitionsPage: async ({ page }, use, testInfo: TestInfo) => {
+    if (
+      testInfo.project.name === 'chrome-unauth' &&
+      !testInfo.annotations.find((a) => a.type === 'allowUnauthChrome')
+    ) {
+      testInfo.skip();
+    }
     await page.goto(tabsData.VariableDefinitions.route);
     await expect(page).toHaveURL(/\/variable-definitions$/);
     await stabilize();
