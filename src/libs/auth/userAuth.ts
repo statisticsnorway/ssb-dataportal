9cb288fe0f43ba9905e768183b9ffc38160473ff
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2026-04-08 10:02:21 +0200

    Add env vars for local dev use

--- src/libs/auth/userAuth.ts
+++ src/libs/auth/userAuth.ts
@@ -1,5 +1,13 @@
+import 'server-only'
+
 import { Auth } from "@/types/auth";
+import { createLogger } from "../logger/server-logger";
+const logger = createLogger('user-auth');
 
 export function authenticateUser(): Auth {
+  if (process.env.DANGEROUSLY_DISABLE_USER_AUTH === 'true') {
+    logger.warn("Danger! User auth is disabled. This may allow unauthenticated users access. This message should only be visible in local dev environments.")
+    return {isAuthenticated: process.env.IS_AUTHENTICATED === 'true'}
+  }
   return {isAuthenticated: true}
 }
