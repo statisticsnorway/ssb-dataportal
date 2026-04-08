1f3aac8fd82e6fcbddd78b10662177f202adb3db
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-04-08 15:02:07 +0200

    set up test

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -1,9 +1,39 @@
-import test from '@playwright/test';
+import test, { expect } from '@playwright/test';
 import { stabilize } from './utils/commonUtils';
 
 test.describe('Log in and out', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('/');
     stabilize();
+  });
+
+  test('redirects to logout when user logs out', async ({ page }) => {
+    // is not authenticated
+    const logoutButton = page.getByRole('button', { name: 'Logg ut' });
+    await expect(logoutButton).toBeVisible();
+    // logs out
+
+    //await expect(logoutButton).toContainText('Logg ut');
+
+    //await page.getByRole('button', { name: 'Logg ut' }).click();
+
+    //await logoutButton.click();
+    //await expect(page).toHaveURL('/oauth2/logout');
+  });
+  test('redirects to login when user logs in', async ({ page }) => {
+    // is not authenticated
+    const loginDialog = page.getByRole('button', { name: 'Logg inn' });
+    await expect(loginDialog).toBeVisible();
+
+    // skille
+    const loginButton = page.getByRole('button', { name: 'Logg inn' });
+    // dialog
+    // new button
+    // logs out
+
+    //await page.getByRole('button', { name: 'Logg ut' }).click();
+
+    //await logoutButton.click();
+    //await expect(page).toHaveURL('/oauth2/logout');
   });
 });
