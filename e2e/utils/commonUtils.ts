2bbf714f25c71c4c8c6986b9ca780101fa12971a
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-04-08 10:43:51 +0200

    remove unused imports

--- e2e/utils/commonUtils.ts
+++ e2e/utils/commonUtils.ts
@@ -1,5 +1,4 @@
-import { expect, Locator, Page } from '@playwright/test';
-import { variables } from './variables';
+import { expect, Locator } from '@playwright/test';
 
 export async function checkCheckbox(checkboxLocator: Locator) {
   await expect(checkboxLocator).toBeVisible();
