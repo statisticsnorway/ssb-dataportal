1421d4fce74df653c81b15b65e6a0fa8565ca729
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-04-09 16:08:41 +0200

    add helper function

--- e2e/utils/commonUtils.ts
+++ e2e/utils/commonUtils.ts
@@ -1,4 +1,4 @@
-import { expect, Locator } from '@playwright/test';
+import { expect, Locator, Page } from '@playwright/test';
 
 export async function checkCheckbox(checkboxLocator: Locator) {
   await expect(checkboxLocator).toBeVisible();
@@ -9,4 +9,10 @@ export async function checkCheckbox(checkboxLocator: Locator) {
 
 export async function stabilize(ms = 200) {
   await new Promise((r) => setTimeout(r, ms));
+}
+
+export async function expectButtonVisible(page: Page, roleName: string) {
+  const button = page.getByRole('button', { name: roleName });
+  await expect(button).toBeVisible();
+  return button;
 }
