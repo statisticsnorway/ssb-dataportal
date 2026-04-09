fff05ed94ab2db5b26ce5a55ad8243d7fa0ff234
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-04-09 16:08:51 +0200

    simplify tests

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -1,48 +1,62 @@
-import { stabilize } from './utils/commonUtils';
+import { expectButtonVisible, stabilize } from './utils/commonUtils';
 import { test, expect } from './fixtures/unauth.fixture';
+import { localization } from '@/libs/language';
+import { Page } from '@playwright/test';
+
+async function openLoginDialog(page: Page) {
+  const loginButton = await expectButtonVisible(page, localization.authentication.logIn);
+  await loginButton.click();
+  const loginDialog = page.getByRole('dialog');
+  await expect(loginDialog).toBeVisible();
+  return loginDialog;
+}
 
 test.describe('Log in and out', () => {
+  const logInText = localization.authentication.logIn;
+  const logInAsText = localization.authentication.logInAs;
+  const logOutText = localization.authentication.logOut;
+
   test.beforeEach(async ({ page }) => {
     await page.goto('/');
     stabilize();
   });
 
-  test('@unauth redirects to logout when user logs out', async ({ page }, testInfo) => {
-    // is not authenticated
-    if (testInfo.project.name !== 'chrome-unauth') {
-      testInfo.skip();
-    }
-    const logoutButton = page.getByRole('button', { name: 'Logg ut' });
-    await page.pause();
-    console.log(await page.content());
-    console.log(logoutButton);
-    //await expect(logoutButton).toBeVisible();
+  const authButtonTests = [
+    { project: 'chrome-unauth', buttonText: logInText },
+    { project: 'chromium', buttonText: logOutText },
+    { project: 'firefox', buttonText: logOutText },
+  ];
+  authButtonTests.forEach(({ project, buttonText }) => {
+    test(`displays "${buttonText}" button correctly project: ${project}`, async ({ page }, testInfo) => {
+      test.skip(testInfo.project.name !== project);
+      await expectButtonVisible(page, buttonText);
+    });
   });
-  test('redirects to login when user logs in', async ({ page }, testInfo) => {
-    // is not authenticated
-    if (testInfo.project.name === 'chrome-unauth') {
-      testInfo.skip();
-    }
-    const loginDialog = page.getByRole('button', { name: 'Logg inn' });
-    await expect(loginDialog).toBeVisible();
 
-    // skille
-    const loginButton = page.getByRole('button', { name: 'Logg inn' });
-    // dialog
-    // new button
-    // logs out
+  test('log in button opens up dialog box', async ({ page }, testInfo) => {
+    test.skip(testInfo.project.name !== 'chrome-unauth');
+    const loginDialog = await openLoginDialog(page);
+    await expect(loginDialog).toContainText('Innlogging');
+  });
 
-    //await page.getByRole('button', { name: 'Logg ut' }).click();
+  test('log out button redirects to log out page', async ({ page }, testInfo) => {
+    test.skip(testInfo.project.name === 'chrome-unauth');
+    const logoutButton = await expectButtonVisible(page, logOutText);
+    await logoutButton.click();
+    await expect(page).toHaveURL(/oauth2\/logout$/);
+  });
 
-    //await logoutButton.click();
-    //await expect(page).toHaveURL('/oauth2/logout');
-    // logs out
+  test('log in as SSB employee redirects to login page', async ({ page }, testInfo) => {
+    test.skip(testInfo.project.name !== 'chrome-unauth');
 
-    //await expect(logoutButton).toContainText('Logg ut');
+    await test.step('Open login dialog', async () => {
+      await openLoginDialog(page);
+    });
 
-    //await page.getByRole('button', { name: 'Logg ut' }).click();
-
-    //await logoutButton.click();
-    //await expect(page).toHaveURL('/oauth2/logout');
+    await test.step('Click log in as employee', async () => {
+      const finalLoginButton = await expectButtonVisible(page, logInAsText);
+      await finalLoginButton.click();
+      await expect(page).toHaveURL(/oauth2\/login$/);
+    });
   });
 });
