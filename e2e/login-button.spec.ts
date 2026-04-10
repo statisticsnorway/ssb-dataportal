ec26c56fabad24e906e7792a070e91b0e0d0692d
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-04-11 00:05:13 +0200

    do not test text content

--- e2e/login-button.spec.ts
+++ e2e/login-button.spec.ts
@@ -16,9 +16,6 @@ test.describe('Log in and out', () => {
   const logInAsText = localization.authentication.logInSsbEmployee;
   const logOutText = localization.authentication.logOut;
 
-  const dialogHeadingText = localization.authentication.loginHeading;
-  const dialogInfoText = localization.authentication.loginInfo;
-
   test.beforeEach(async ({ page }) => {
     await page.goto('/');
     stabilize();
@@ -41,8 +38,8 @@ test.describe('Log in and out', () => {
     const loginDialog = await openLoginDialog(page);
     const dialogHeading = loginDialog.getByRole('heading');
     const dialogInfo = loginDialog.getByRole('paragraph');
-    await expect(dialogHeading).toContainText(dialogHeadingText);
-    await expect(dialogInfo).toContainText(dialogInfoText);
+    await expect(dialogHeading).toBeVisible();
+    await expect(dialogInfo).toBeVisible();
   });
 
   test('log out button redirects to log out page', async ({ page }, testInfo) => {
