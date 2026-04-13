3dc69f148cce9c8e2574c586ab9950732c2abf99
Jan <63044278+JanhSander@users.noreply.github.com>
2026-04-13 20:45:21 +0200

    Replace hardcoded text

--- src/app/(details)/variable-definitions/[shortName]/not-found.tsx
+++ src/app/(details)/variable-definitions/[shortName]/not-found.tsx
@@ -11,7 +11,7 @@ export default function NotFound() {
       helpList={localization.error.notFoundHelpListVariableDetails}
       homeHref='/'
       secondaryHref='/variable-definitions'
-      secondaryLabel='Variabeldefinisjoner'
+      secondaryLabel={localization.variableDefinition.labelPlural}
       showBrokenLinkButton={false}
     />
   );
