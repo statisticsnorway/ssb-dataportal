f2cdd4f5cd3886f8707f594eb769027cc56613b8
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-04-17 14:45:49 +0200

    this could probably use nuqs

--- src/components/login-button/index.tsx
+++ src/components/login-button/index.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Button, Dialog, Heading } from '@digdir/designsystemet-react';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { useAuthContext } from '@/app/authContext';
 import { localization } from '@/libs/language/src/localization';
 
@@ -18,20 +18,25 @@ import { localization } from '@/libs/language/src/localization';
 const LoginButton = () => {
   const { isAuthenticated } = useAuthContext();
   const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
+  const basePage = pathname + (searchParams.toString() ? `?${searchParams}` : '');
+  // biome-ignore lint/suspicious/noConsole: <explanation>
+  console.log(basePage);
+  const urlLogOut = new URL(String(process.env.NEXT_PUBLIC_LOGOUT_URL));
+  urlLogOut.searchParams.set('next', basePage);
+  const urlLogIn = new URL(String(process.env.NEXT_PUBLIC_LOGIN_URL));
+  urlLogIn.searchParams.set('next', basePage);
   return isAuthenticated ? (
-    <Button onClick={() => router.push(String(process.env.NEXT_PUBLIC_LOGOUT_URL))}>
-      {localization.authentication.logOut}
-    </Button>
+    <Button onClick={() => router.push(urlLogOut.toString())}>{localization.authentication.logOut}</Button>
   ) : (
     <Dialog.TriggerContext>
       <Dialog.Trigger>{localization.authentication.logIn}</Dialog.Trigger>
       <Dialog>
         <Heading level={3}>{localization.authentication.loginHeading}</Heading>
         <p>{localization.authentication.loginInfo}</p>
-        <Button onClick={() => router.push(String(process.env.NEXT_PUBLIC_LOGIN_URL))}>
-          {localization.authentication.logInSsbEmployee}
-        </Button>
+        <Button onClick={() => router.push(urlLogIn.toString())}>{localization.authentication.logInSsbEmployee}</Button>
       </Dialog>
     </Dialog.TriggerContext>
   );
