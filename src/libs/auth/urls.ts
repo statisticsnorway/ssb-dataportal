0deb8527f53ae2d06f5a1ed673af191f2ab4b4f7
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-04-20 22:08:55 +0200

    correct functions

--- src/libs/auth/urls.ts
+++ src/libs/auth/urls.ts
@@ -1,19 +1,11 @@
 export function getLoginUrl(pathname: string): string {
   const base = process.env.NEXT_PUBLIC_LOGIN_URL;
   if (!base) throw new Error('Missing LOGIN URL');
-
-  const url = new URL(base);
-  url.searchParams.set('redirect', pathname.startsWith('/') ? pathname : '/');
-
-  return url.toString();
+  return `${base}?redirect=${pathname}`;
 }
 
 export function getLogoutUrl(pathname: string): string {
   const base = process.env.NEXT_PUBLIC_LOGOUT_URL;
   if (!base) throw new Error('Missing LOGOUT URL');
-
-  const url = new URL(base);
-  url.searchParams.set('redirect', pathname.startsWith('/') ? pathname : '/');
-
-  return url.toString();
+  return `${base}?redirect=${pathname}`;
 }
