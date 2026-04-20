d1c63e21e96b4edb805c6c943e1fd011fe1bd5d8
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-04-20 15:40:27 +0200

    use NEXT PUBLIC is rendered in client component

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,9 +3,9 @@ export function getVardefApiDocsUrl(): string {
 }
 
 export function getLoginUrl(pathname: String): string {
-  return `${process.env.LOGIN_URL}?redirect=${pathname}`;
+  return `${process.env.NEXT_PUBLIC_LOGIN_URL}?redirect=${pathname}`;
 }
 
 export function getLogoutUrl(pathname: String): string {
-  return `${process.env.LOGOUT_URL}?redirect=${pathname}`;
+  return `${process.env.NEXT_PUBLIC_LOGOUT_URL}?redirect=${pathname}`;
 }
