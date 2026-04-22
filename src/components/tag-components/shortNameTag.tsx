81e8876413de3c408b9f301d8c7e2194e03654c5
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-04-22 23:32:33 +0200

    add tooltip and copy

--- src/components/tag-components/shortNameTag.tsx
+++ src/components/tag-components/shortNameTag.tsx
@@ -1,4 +1,4 @@
-import { Tag } from '@digdir/designsystemet-react';
+import { Tag, Tooltip } from '@digdir/designsystemet-react';
 import { FilesIcon } from '@navikt/aksel-icons';
 import { localization } from '@/libs/language/src/localization';
 
@@ -6,14 +6,32 @@ interface ShortNameTagProps {
   className?: string;
   shortName: string;
   copy?: boolean;
+  copied: boolean;
+  copyToClipboard: (text: string) => Promise<void>;
 }
 
-const ShortNameTag = ({ className, shortName, copy = true }: ShortNameTagProps) => {
+const ShortNameTag = ({ className, shortName, copy = true, copied, copyToClipboard }: ShortNameTagProps) => {
   return copy ? (
     <Tag data-size='lg' data-color='success' className={className} aria-label={localization.shortName.label}>
-      <span style={{ display: 'flex', gap: '1rem' }}>
+      <span style={{ display: 'flex', gap: '1rem', alignItems: 'center' }}>
         {shortName}
-        <FilesIcon focusable={true} />
+
+        <span
+          role='button'
+          tabIndex={0}
+          aria-label={copied ? localization.copy.copied : localization.copy.shortName}
+          onClick={() => copyToClipboard(shortName)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter' || e.key === ' ') {
+              e.preventDefault();
+              copyToClipboard(shortName);
+            }
+          }}
+        >
+          <Tooltip content={copied ? localization.copy.copied : localization.copy.shortName}>
+            <FilesIcon aria-hidden />
+          </Tooltip>
+        </span>
       </span>
     </Tag>
   ) : (
