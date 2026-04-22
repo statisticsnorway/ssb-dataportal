c29d541925cc0d1e5d285f605503990a48b75b9e
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2026-04-22 15:47:52 +0200

    Update footer layout (#295)
    * New structure for footer
    
    * Resolve alignment and padding
    
    * Responsive margins
    
    * Fix tests
    
    * Use localization

---
+++ src/components/feedback-callout/index.tsx
@@ -0,0 +1,19 @@
+import { Card, Heading, Paragraph } from '@digdir/designsystemet-react';
+import { ExternalLink } from '@/components/external-link';
+import { localization } from '@/libs/language';
+import styles from './feedback-callout.module.css';
+
+export const FeedbackCallout = () => {
+  return (
+    <Card role='complementary' className={styles.infoSection}>
+      <Heading>{localization.info.feedbackTitle}</Heading>
+      <Paragraph>
+        {localization.info.feedbackBody}{' '}
+        <ExternalLink
+          linkText={localization.info.feedBackForm}
+          href='https://forms.office.com/Pages/ResponsePage.aspx?id=knAhx0CyHU69YfqXupdcvG8mQNraR5ZAu3es4-se84xUN0VFME5BSVFSUTZDRUZCTzNTVUlFTDlUNC4u'
+        />
+      </Paragraph>
+    </Card>
+  );
+};
