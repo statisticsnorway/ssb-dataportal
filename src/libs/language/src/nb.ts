d9def5555278272035b3c72b8fd7fbaf1f310254
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2026-04-22 09:50:54 +0200

    Update text om landing page

--- src/libs/language/src/nb.ts
+++ src/libs/language/src/nb.ts
@@ -15,7 +15,7 @@ export const nb = {
   daplaManual: 'Dapla-manualen',
   documentation: 'Dokumentasjon',
   apiDocumentation: 'API-dokumentasjon',
-  feedBackForm: 'tilbakemeldingsskjema for Dataportalen',
+  feedBackForm: 'tilbakemeldingsskjema for SSB Dataportal',
   from: 'Fra',
   home: 'Hjem',
   id: 'ID',
@@ -77,10 +77,10 @@ export const nb = {
     helpChangeFilters: 'endre søk eller filtre',
     supportPrefix: 'Har problemet vart en stund, kan du',
     supportLinkText: 'kontakte oss',
-    brokenLinkMailSubject: 'Ødelagt lenke i dataportalen',
+    brokenLinkMailSubject: 'Ødelagt lenke i SSB dataportal',
     brokenLinkMailBody: (path: string) =>
       [
-        'Hei, jeg vil melde fra om en mulig ødelagt lenke i Dataportalen.',
+        'Hei, jeg vil melde fra om en mulig ødelagt lenke i SSB Dataportal.',
         '',
         `Side: ${path}`,
         '',
@@ -133,7 +133,7 @@ export const nb = {
     hits: 'treff',
     label: 'Søk',
     noHits: 'Ditt søk ga ingen treff',
-    searchDataportal: 'Søk i Dataportalen',
+    searchDataportal: 'Søk i SSB Dataportal',
     searchForClassifications: 'Søk i kodeverk',
     searchForDatasets: 'Søk i datasett',
     searchForVariableDefinitions: 'Søk i variabeldefinisjoner',
@@ -183,15 +183,15 @@ export const nb = {
     datasetProtoypeInfo:
       'Denne siden vil i fremtiden inneholde vår nye løsning for beskrivelser av datasett. Utviklingen starter for fullt i 2026.',
     landingPageInfoGoal:
-      'Målet er å gjøre det enklere for alle i SSB å finne, forstå og bruke dataene våre på en korrekt og effektiv måte.',
-    landingPageInfoGoalTitle: 'Hvorfor Dataportalen?',
+      'Målet er å gjøre det enklere å finne, forstå og bruke SSBs dataer på en korrekt og effektiv måte.',
+    landingPageInfoGoalTitle: 'Hvorfor SSB Dataportal?',
     landingPageInfoIntro: 'Her samler vi informasjon om våre datasett, variabler, kodeverk og API-er på ett sted.',
-    landingPageInfoIntroTitle: 'Hva er Dataportalen?',
+    landingPageInfoIntroTitle: 'Hva er SSB Dataportal?',
     landingPageInfoPrototype:
-      'I denne første versjonen kan du utforske SSBs variabeldefinisjoner. Portalen vil gradvis utvides med oversikt over datasett, kodeverk og API-er, slik at du kan se hele sammenhengen fra konsept til ferdig datafil. Ved å koble definisjoner fra Vardef med kodeverk fra Klass og dokumentasjon fra Datadoc, etablerer vi en "felles sannhet" som sikrer at vi alltid tolker og bruker dataene våre på samme måte.',
-    landingPagePrototypeTitle: 'Dataportal prototype',
+      'I denne første versjonen kan du utforske SSBs variabeldefinisjoner. Portalen vil gradvis utvides med oversikt over datasett, kodeverk og API-er, slik at du kan se hele sammenhengen fra konsept til ferdig datafil. Ved å koble definisjoner fra Vardef med kodeverk fra Klass og dokumentasjon om datasett, etablerer vi en "felles sannhet" som sikrer at vi alltid tolker og bruker dataene våre på samme måte.',
+    landingPagePrototypeTitle: 'Kontinuerlig forbedring',
     landingPageSubTitle: 'Din inngang til SSBs felles kunnskap om data',
-    landingPageTitle: 'Velkommen til Dataportalen',
+    landingPageTitle: 'Velkommen til SSB Dataportal',
   },
 
   loading: {
