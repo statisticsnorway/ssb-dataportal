b70e0405c2334ca6caa2b03ecc7f7f8a74e1d1fc
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2026-04-22 16:25:10 +0200

    Colors as per design spec

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -14,31 +14,52 @@ export const Footer = () => {
   return (
     <footer className={styles.footer}>
       <section className={styles.footerLinkSection}>
-        <Heading>{localization.appTitle}</Heading>
-        <p>{localization.info.landingPageSubTitle}</p>
+        <Heading level={4} className={'negative-text'}>
+          {localization.appTitle}
+        </Heading>
+        <p className={'negative-text'}>{localization.info.landingPageSubTitle}</p>
       </section>
       <section className={styles.footerLinkSection}>
-        <Heading>{localization.info.footerAboutPage}</Heading>
-        <FooterLinkItem icon={<ShieldLockIcon title={localization?.contact?.label} fontSize='2rem' />}>
+        <Heading level={4} className={'negative-text'}>
+          {localization.info.footerAboutPage}
+        </Heading>
+        <FooterLinkItem
+          icon={<ShieldLockIcon title={localization?.contact?.label} fontSize='2rem' className={styles.negativeLink} />}
+        >
           <ExternalLink
             href='https://www.ssb.no/omssb/personvern/personvernerklaering'
             linkText={localization.info.footerPrivacyStatement}
+            className={styles.negativeLink}
           />
         </FooterLinkItem>
-        <FooterLinkItem icon={<FigureIcon title={localization?.contact?.label} fontSize='2rem' />}>
-          <p>{localization.info.footerAccessibilityStatement}</p>
+        <FooterLinkItem
+          icon={<FigureIcon title={localization?.contact?.label} fontSize='2rem' className={styles.negativeLink} />}
+        >
+          <p className={styles.negativeLink}>{localization.info.footerAccessibilityStatement}</p>
         </FooterLinkItem>
-        <FooterLinkItem icon={<BookIcon title={localization.apiDocumentation} fontSize='2rem' />}>
-          <ApiDocLink href={apiDocsUrl} />
+        <FooterLinkItem
+          icon={<BookIcon title={localization.apiDocumentation} fontSize='2rem' className={styles.negativeLink} />}
+        >
+          <ApiDocLink href={apiDocsUrl} className={styles.negativeLink} />
         </FooterLinkItem>
       </section>
       <section className={styles.footerLinkSection}>
-        <Heading>{localization.info.footerContact}</Heading>
-        <FooterLinkItem icon={<EnvelopeOpenIcon title={localization?.contact?.label} fontSize='2rem' />}>
-          <Link href={`mailto:${getContactEmailAddress()}`}>{getContactEmailAddress()}</Link>
+        <Heading level={4} className={'negative-text'}>
+          {localization.info.footerContact}
+        </Heading>
+        <FooterLinkItem
+          icon={
+            <EnvelopeOpenIcon title={localization?.contact?.label} fontSize='2rem' className={styles.negativeLink} />
+          }
+        >
+          <Link className={styles.negativeLink} href={`mailto:${getContactEmailAddress()}`}>
+            {getContactEmailAddress()}
+          </Link>
         </FooterLinkItem>
-        <FooterLinkItem icon={<HouseIcon title={localization?.contact?.label} fontSize='2rem' />}>
-          <ExternalLink href='https://www.ssb.no' linkText='ssb.no' />
+        <FooterLinkItem
+          icon={<HouseIcon title={localization?.contact?.label} fontSize='2rem' className={styles.negativeLink} />}
+        >
+          <ExternalLink href='https://www.ssb.no' linkText='ssb.no' className={styles.negativeLink} />
         </FooterLinkItem>
       </section>
     </footer>
