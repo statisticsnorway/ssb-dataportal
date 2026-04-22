1180b1707369944a53057806e5669f7087afee2d
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2026-04-22 14:21:44 +0200

    Responsive margins

--- src/components/feedback-callout/index.tsx
+++ src/components/feedback-callout/index.tsx
@@ -1,4 +1,4 @@
-import { Card, Paragraph } from '@digdir/designsystemet-react';
+import { Card, Heading, Paragraph } from '@digdir/designsystemet-react';
 import { ExternalLink } from '@/components/external-link';
 import { localization } from '@/libs/language';
 import styles from './feedback-callout.module.css';
@@ -6,9 +6,9 @@ import styles from './feedback-callout.module.css';
 export const FeedbackCallout = () => {
   return (
     <Card className={styles.infoSection}>
-      <img src={'/pencil.svg'} alt='' width={50} className={styles.iconImage} />
+      <Heading>Del dine erfaringer!</Heading>
       <Paragraph>
-        Dataportalen er under utvikling, og dine innspill er viktige for oss. Hjelp oss gjerne ved å fylle ut{' '}
+        Vi jobber kontinuerlig med å forbedre dataportalen og du kan hjelpe oss ved å fylle ut{' '}
         <ExternalLink
           linkText={localization.feedBackForm}
           href='https://forms.office.com/Pages/ResponsePage.aspx?id=knAhx0CyHU69YfqXupdcvG8mQNraR5ZAu3es4-se84xUN0VFME5BSVFSUTZDRUZCTzNTVUlFTDlUNC4u'
