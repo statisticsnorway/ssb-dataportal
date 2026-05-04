754ca599c588b5d24a9ff3922e9337d7784ccefa
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-05-04 18:03:21 +0200

    conditionally use link in logo - add placement to logo alt text to not confuse screen readers

--- src/components/dataportal-logo/index.tsx
+++ src/components/dataportal-logo/index.tsx
@@ -7,14 +7,33 @@ export interface LogoProps {
   homeUrl?: string;
   title?: string;
   negative?: boolean;
+  placement?: string;
 }
 
-export const DataportalLogo = ({ homeUrl, title, negative }: LogoProps) => {
+export const DataportalLogo = ({ homeUrl, title, negative, placement }: LogoProps) => {
   const imageNameBase = process.env.USE_ANNIVERSARY_LOGO === 'true' ? 'ssb_logo_anniversary_no' : 'ssb_logo';
   const imageFile = negative ? `/${imageNameBase}_light.svg` : `/${imageNameBase}_dark.svg`;
   const textColor = negative ? `${styles.logoAndTitle} negative-text` : styles.logoAndTitle;
-  return (
+  return homeUrl ? (
     <Link href={homeUrl} title={localization.navigateHome} className={textColor}>
+      <Image
+        className={styles.logo}
+        src={imageFile}
+        alt={`${localization.statisticsNorway} ${placement} logo`}
+        width={1435}
+        height={929}
+        sizes='(min-width: 768px) 248px, (max-width: 768px) 180px'
+        priority
+      />
+      {title ? (
+        <div className={textColor}>
+          <div className={styles.verticalDivider} />
+          <div className={styles.appTitle}>{title}</div>
+        </div>
+      ) : undefined}
+    </Link>
+  ) : (
+    <div className={textColor}>
       <Image
         className={styles.logo}
         src={imageFile}
@@ -30,6 +49,46 @@ export const DataportalLogo = ({ homeUrl, title, negative }: LogoProps) => {
           <div className={styles.appTitle}>{title}</div>
         </div>
       ) : undefined}
-    </Link>
+    </div>
   );
 };
+
+/*
+return homeUrl ? (
+  <Link href={homeUrl} title={localization.navigateHome} className={textColor}>
+    <Image
+      className={styles.logo}
+      src={imageFile}
+      alt={`${localization.statisticsNorway} logo`}
+      width={1435}
+      height={929}
+      sizes="(min-width: 768px) 248px, (max-width: 768px) 180px"
+      priority
+    />
+    {title ? (
+      <div className={textColor}>
+        <div className={styles.verticalDivider} />
+        <div className={styles.appTitle}>{title}</div>
+      </div>
+    ) : null}
+  </Link>
+) : (
+  <div className={textColor}>
+    <Image
+      className={styles.logo}
+      src={imageFile}
+      alt={`${localization.statisticsNorway} logo`}
+      width={1435}
+      height={929}
+      sizes="(min-width: 768px) 248px, (max-width: 768px) 180px"
+      priority
+    />
+    {title ? (
+      <div className={textColor}>
+        <div className={styles.verticalDivider} />
+        <div className={styles.appTitle}>{title}</div>
+      </div>
+    ) : null}
+  </div>
+);
+*/
