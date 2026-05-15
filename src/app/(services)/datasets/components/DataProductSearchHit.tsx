0e0f3bb1e13d7a90e3b22d71194acfbf36ff3c15
Jorgen-5 <rlj@ssb.no>
2026-05-15 12:51:46 +0200

    Update component

--- src/app/(services)/datasets/components/DataProductSearchHit.tsx
+++ src/app/(services)/datasets/components/DataProductSearchHit.tsx
@@ -1,23 +1,33 @@
 import { Card, Heading, Link, Tag } from '@digdir/designsystemet-react';
+import { type ReactNode } from 'react';
+
 import { tabsData } from '@/app/(services)/tabs';
 import { DataProductDTO } from '@/libs/data-access/datadoc/models';
 
 import styles from './dataProduct.module.css';
 
-export const DataProductSearchHit = ({ dataProduct }: { dataProduct: DataProductDTO }) => (
-  <Card>
-    <Heading data-size='md'>
-      <Link
-        href={`${tabsData.Datasets.route}/${dataProduct.product_short_name}`}
-        className={styles.dataProductHeadingLink}
-      >
-        {dataProduct.title}
-      </Link>
-    </Heading>
-    <div className={styles.tagContainer}>
-      <Tag data-color='success' data-size='md'>
-        {dataProduct.product_short_name}
-      </Tag>
-    </div>
-  </Card>
+interface HeadingLinkProps {
+  href: string;
+  children: ReactNode;
+}
+
+const HeadingLink = ({ href, children }: HeadingLinkProps) => (
+  <Link href={href} className={styles.dataProductHeadingLink}>
+    {children}
+  </Link>
 );
+
+export const DataProductSearchHit = ({ dataProduct }: { dataProduct: DataProductDTO }) => {
+  const dataProductRoute = `${tabsData.Datasets.route}/${dataProduct.product_short_name}`;
+
+  return (
+    <Card data-testid='data-product-search-card'>
+      <Heading data-size='md' className={styles.dataProductHeadingLink}>
+        <HeadingLink href={dataProductRoute}>
+          <span className='heading12'>{dataProduct.title}</span>
+        </HeadingLink>
+      </Heading>
+      <Tag>{dataProduct.product_short_name}</Tag>
+    </Card>
+  );
+};
