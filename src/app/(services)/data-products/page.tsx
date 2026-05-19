11d24cb7ce19ad77c67024df7bc0ec9b61478a83
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2026-05-19 09:01:38 +0200

    Data products page not static (#359)
    This is dependent on fetching from an external API which we
    haven't configured during builds.

--- src/app/(services)/data-products/page.tsx
+++ src/app/(services)/data-products/page.tsx
@@ -3,6 +3,7 @@ import { Metadata } from 'next';
 import { SearchPage } from '@/components/search-page-wrapper/search-page';
 import { listDataProducts } from '@/libs/data/datasets/datasets';
 import { localization } from '@/libs/language';
+import { createLogger } from '@/libs/logger/server-logger';
 import { tabsData } from '../tabs';
 import { DataProductSearchHit } from './components/DataProductSearchHit';
 import styles from './page.module.css';
@@ -11,7 +12,14 @@ export const metadata: Metadata = {
   title: localization.pageTitle.dataProducts,
 };
 
-export default async function Datasets() {
+export default async function Datasets({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+  const params = await searchParams;
+  const logger = createLogger('data-products-discover-page');
+  logger.info({ params }, 'Data products page access');
   const dataProducts = await listDataProducts();
   const pageInfo = (
     <Alert data-color='info' data-size='lg'>
