21abb31074d5b98f07ff250741fed0ac9bc5d371
Jan <63044278+JanhSander@users.noreply.github.com>
2026-05-20 20:34:44 +0200

    Add force-dynamic

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,5 +1,7 @@
 import type { MetadataRoute } from 'next';
 
+export const dynamic = 'force-dynamic';
+
 export default function robots(): MetadataRoute.Robots {
   const siteUrl = process.env.SITE_URL ?? 'https://dataportal.ssb.no';
   return {
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,7 @@
 import type { MetadataRoute } from 'next';
 
+export const dynamic = 'force-dynamic';
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const siteUrl = process.env.SITE_URL ?? 'https://dataportal.ssb.no';
   return [
