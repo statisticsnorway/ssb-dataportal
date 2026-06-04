fd982110dbb3084d7b5c575e7419e84900eae011
Jorgen-5 <rlj@ssb.no>
2026-06-04 15:33:11 +0200

    Rename file

--- src/components/data-coverage-timeline/useTimelineData.tsx
+++ src/components/data-coverage-timeline/useTimelineData.tsx
@@ -1,7 +1,7 @@
 import { useMemo } from 'react';
 import { type DaplaDataFileDTO, PeriodFormat } from '@/libs/data-access/datadoc';
 import { clientLogger } from '@/libs/logger/client-logger';
-import { generateYearSlots } from './periodDefs';
+import { generateYearSlots } from './periodDefinitions';
 import { type Slot, type TimelineItem } from './types';
 import { pad2 } from './utils';
 
