eaf6516f9c0496424cc442e7d466d742bbe55494
Jorgen-5 <rlj@ssb.no>
2026-06-11 13:12:18 +0200

    Change subjcet field lookup

--- src/utils/subjectFiledsMapping.ts
+++ src/utils/subjectFiledsMapping.ts
@@ -1,27 +1,21 @@
-export type SubjectFieldsMapping = {
-  id: number;
-  code: string;
+export const SUBJECT_FIELD_BY_CODE: Record<string, number[]> = {
+  al: [1],
+  bf: [2],
+  be: [3, 8],
+  bb: [4],
+  ei: [5],
+  he: [6],
+  js: [9],
+  nk: [11],
+  nm: [12],
+  os: [13],
+  pp: [14],
+  sk: [16],
+  ti: [18],
+  tr: [19],
+  ud: [20],
+  ut: [21],
+  va: [22],
+  vt: [23],
+  vf: [24],
 };
-
-export const SUBJECTFILEDS_MAPPING: SubjectFieldsMapping[] = [
-  { id: 1, code: 'al' },
-  { id: 2, code: 'bf' },
-  { id: 3, code: 'be' },
-  { id: 4, code: 'bb' },
-  { id: 5, code: 'ei' },
-  { id: 6, code: 'he' },
-  { id: 8, code: 'be' },
-  { id: 9, code: 'js' },
-  { id: 11, code: 'nk' },
-  { id: 12, code: 'nm' },
-  { id: 13, code: 'os' },
-  { id: 14, code: 'pp' },
-  { id: 16, code: 'sk' },
-  { id: 18, code: 'ti' },
-  { id: 19, code: 'tr' },
-  { id: 20, code: 'ud' },
-  { id: 21, code: 'ut' },
-  { id: 22, code: 'va' },
-  { id: 23, code: 'vt' },
-  { id: 24, code: 'vf' },
-];
