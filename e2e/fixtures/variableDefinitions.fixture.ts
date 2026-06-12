0b48f31fe8c38fe45cadd89b1a8bc658bc0dcdb7
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-06-12 11:51:06 +0200

    rename 'use' according to typescript rules for React

--- e2e/fixtures/variableDefinitions.fixture.ts
+++ e2e/fixtures/variableDefinitions.fixture.ts
@@ -6,12 +6,12 @@ import { stabilize } from '../utils/commonUtils';
 export const test = base.extend<{
   variableDefinitionsPage: Page;
 }>({
-  variableDefinitionsPage: async ({ page }, use, testInfo: TestInfo) => {
+  variableDefinitionsPage: async ({ page }, useVariableDefinitionsPage, testInfo: TestInfo) => {
     test.skip(testInfo.project.name === 'chrome-unauth');
     await page.goto(tabsData.VariableDefinitions.route);
     await expect(page).toHaveURL(/\/variable-definitions$/);
     await stabilize();
-    await use(page);
+    await useVariableDefinitionsPage(page);
   },
 });
 export { expect };
