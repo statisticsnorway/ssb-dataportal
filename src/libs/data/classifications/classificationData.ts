9e2e8935918cae8789e2627f617aad1e6d1c91dc
Jorgen-5 <rlj@ssb.no>
2026-06-16 10:30:02 +0200

    Only fetch norwegian

--- src/libs/data/classifications/classificationData.ts
+++ src/libs/data/classifications/classificationData.ts
@@ -1,8 +1,10 @@
 'use server';
 
 import {
+  ClassificationLanguageEnum,
   ClassificationRequest,
   ClassificationsApi,
+  ClassificationsLanguageEnum,
   ClassificationsRequest,
 } from '@/libs/data-access/klass/apis/ClassificationsApi';
 import { SearchApi, SearchRequest } from '@/libs/data-access/klass/apis/SearchApi';
@@ -89,6 +91,7 @@ export async function fetchAllClassifications(): Promise<ClassificationResource[
 
   const params = {
     includeCodelists: true,
+    language: ClassificationsLanguageEnum.NB,
   } satisfies ClassificationsRequest;
 
   try {
@@ -138,6 +141,7 @@ export async function fetchClassificationById(id: number): Promise<Classificatio
     const api = await getKlassClassificationsClient();
     const params = {
       id,
+      language: ClassificationLanguageEnum.NB,
     } satisfies ClassificationRequest;
 
     try {
