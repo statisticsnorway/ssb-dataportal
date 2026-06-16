f12cb8ebedb8c4708361a3ace4651a5ca42ff7fe
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-06-16 10:13:37 +0200

    add constants for static urls

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,4 +1,5 @@
 import { Divider, Heading } from '@digdir/designsystemet-react';
+import { ACCESSIBILITY_STATEMENT_URL, PRIVACY_STATEMENT_URL } from '@/config/constants';
 import { localization } from '@/libs/language';
 import { getVardefApiDocsUrl } from '@/utils/config';
 import { getContactEmailAddress } from '@/utils/userAgent';
@@ -31,12 +32,12 @@ export const Footer = () => {
               {localization.info.footerAboutPage}
             </Heading>
             <ExternalLink
-              href='https://www.ssb.no/omssb/personvern/personvernerklaering'
+              href={PRIVACY_STATEMENT_URL}
               linkText={localization.info.footerPrivacyStatement}
               className={styles.negativeLink}
             />
             <ExternalLink
-              href='https://uustatus.no/nb/erklaringer/publisert/b2897950-c57a-4dee-9c34-4668f92b7d8f'
+              href={ACCESSIBILITY_STATEMENT_URL}
               linkText={localization.info.footerAccessibilityStatement}
               className={styles.negativeLink}
             />
