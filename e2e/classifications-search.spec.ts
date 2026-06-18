bf908e4da5d108f6ec7ef4fbe6b018dfac5e6fca
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-06-18 10:57:21 +0200

    add tests

--- e2e/classifications-search.spec.ts
+++ e2e/classifications-search.spec.ts
@@ -1,22 +1,59 @@
-// Næringsgrupp
-// student
-// sort search result by search score
-// jordbær
-// næringsgruppering
-// tags label with title
-// sortert etter search score
-// sort after
-// rangering
-
 import { localization } from '@/libs/language';
 import { expect, test } from './fixtures/classifications.fixture';
+import searchResultsMock from '@/static-data/klass-search-results.json';
+
+const searchTerm = 'Næring';
+const searchTermVariantOne = 'Næringsgrupp';
+const searchTermVariantTwo = 'Næringsgruppering';
+const searchResults = searchResultsMock;
 
 test('Search box is visible', async ({ classificationsPage }) => {
   const searchBox = classificationsPage.getByRole('searchbox', { name: `${localization.search.label}` });
   await expect(searchBox).toBeVisible();
 });
 
-// returns expected result
-// sorts by search score
-// filter tags
-// filter codelists
+// Næringsgrupp
+// student
+// jordbær
+// næringsgruppering
+
+test('Search term returns results', async ({ classificationsPage }) => {
+  const main = classificationsPage.getByRole('main');
+  const searchBox = classificationsPage.getByRole('searchbox', { name: `${localization.search.label}` });
+  await searchBox.fill(searchTerm);
+  await expect(main).toContainText('3 treff');
+  const firstSearchResult = classificationsPage.getByTestId('search-card').first();
+  await expect(firstSearchResult).toBeVisible();
+  await expect(firstSearchResult).toContainText(searchResults[0]!.name);
+  // substring
+  //await expect(firstSearchResult.getByRole('heading', { name: searchResults[0]!.name })).toContainText(searchTerm);
+});
+
+test('Search displays tag', async ({ classificationsPage }) => {
+  const searchBox = classificationsPage.getByRole('searchbox', { name: `${localization.search.label}` });
+  await searchBox.fill(searchTerm);
+  const resultTag = classificationsPage.getByRole('button', { name: `"${searchTerm}"` });
+  await expect(resultTag).toBeVisible();
+});
+
+test('Remove search by tag', async ({ classificationsPage }) => {
+  const main = classificationsPage.getByRole('main');
+  const searchBox = classificationsPage.getByRole('searchbox', { name: `${localization.search.label}` });
+  await searchBox.fill(searchTerm);
+  await expect(main).toContainText('3 treff');
+  const resultTag = classificationsPage.getByRole('button', { name: `"${searchTerm}"` });
+  await resultTag.click();
+  await expect(main).not.toContainText('3 treff');
+  await expect(resultTag).not.toBeInViewport();
+});
+
+test('Search result is sorted by search score', async ({ classificationsPage }) => {});
+
+test('Search result can be sorted by different criteria', async ({ classificationsPage }) => {});
+
+test('Search result can be filtered by type', async ({ classificationsPage }) => {});
+
+test('Search result can be filtered by subject', async ({ classificationsPage }) => {});
+
+// language ?
+// maps ?
