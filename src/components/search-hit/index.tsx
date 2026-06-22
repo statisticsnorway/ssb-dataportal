a1206c2b8c5b1c74f0c7650c35b0b0437395b347
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-06-22 15:55:46 +0200

    use design suystem

--- src/components/search-hit/index.tsx
+++ src/components/search-hit/index.tsx
@@ -1,4 +1,4 @@
-import { Card, Heading, Link, Paragraph } from '@digdir/designsystemet-react';
+import { Card, Heading, Link, Paragraph } from '@statisticsnorway/design-react';
 import { ReactNode } from 'react';
 import styles from './search-hit.module.css';
 
