4ecaeecfc0cb3b3f1f20d4a7b00648f6a92396fc
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2026-06-25 12:29:53 +0200

    Simplify preferred language parsing loop

--- src/utils/header.ts
+++ src/utils/header.ts
@@ -5,9 +5,5 @@ export const parsePreferredLanguage = (headerValue: string): string => {
     .map((lang) => (lang.split(';')[0] ?? '').trim().toLowerCase())
     .map((lang) => lang.slice(0, 2));
 
-  for (const lang of languages) {
-    return lang;
-  }
-
-  return 'nb';
+  return languages[0] ?? 'nb';
 };
