06a7331b9938cd9d8307084e7df4cbe44f15d461
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-07-01 15:47:55 +0200

    fix unwrap params and add simple styling with breadcrumbs

--- src/app/(details)/classifications/[id]/page.tsx
+++ src/app/(details)/classifications/[id]/page.tsx
@@ -4,6 +4,6 @@ import { createLogger } from '@/libs/logger/server-logger';
 export default async function ClassificationPage({ params }: Readonly<{ params: Promise<{ id: string }> }>) {
   const { id } = await params;
   const logger = createLogger('classification-details-page');
-  logger.info({ params }, 'Classification detail page access');
+  logger.info({ id }, 'Classification detail page access');
   redirect(`/classifications/${id}/codes`);
 }
