456f92ffd3e1f031bc65b97c7fe8f7a9aadcf11d
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-07-01 16:32:01 +0200

    add navigation tests

--- e2e/classification-navigation.spec.ts
+++ e2e/classification-navigation.spec.ts
@@ -5,7 +5,23 @@ const classifications = classificationsMock.classifications;
 
 test('Can navigate to classification', async ({ classificationsPage }) => {
   const classification = parseClassification(classifications[0]);
-  const link = classificationsPage.getByRole('link', { name: classification.name! });
+  const link = classificationsPage
+    .getByRole('link', { name: stripTitlePrefix(classification.name!), exact: true })
+    .first();
   await link.click();
+  await classificationsPage.waitForURL(`/classifications/${classification.id}/codes`);
   await expect(classificationsPage.getByRole('heading', { name: classification.name! })).toBeVisible();
+});
+
+test('Can navigate back from classification details page', async ({ classificationsPage }) => {
+  const classification = parseClassification(classifications[0]);
+  const link = classificationsPage
+    .getByRole('link', { name: stripTitlePrefix(classification.name!), exact: true })
+    .first();
+  await link.click();
+  await classificationsPage.waitForURL(`/classifications/${classification.id}/codes`);
+  const linkHome = classificationsPage.getByLabel('Du er her:').getByRole('link', { name: 'Klassifikasjoner' }).getByRole('link', { name: 'Klassifikasjoner' });
+  await expect(linkHome).toBeVisible();
+  await linkHome.click();
+  await expect(classificationsPage).toHaveURL(`/classifications?types=Standard`);
 });
