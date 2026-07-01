617e7db1d81fe0a577a7be2c18a0549bffd48239
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-07-01 13:04:10 +0200

    simple classification fetch

--- src/app/(details)/classifications/[id]/classificationDetail.tsx
+++ src/app/(details)/classifications/[id]/classificationDetail.tsx
@@ -1,16 +1,20 @@
-import { Alert } from '@digdir/designsystemet-react';
+import { Alert, Heading } from '@digdir/designsystemet-react';
 import { BreadcrumbItem, DataportalBreadcrumbs } from '@/components/dataportal-breadcrumbs';
+import { ClassificationResource } from '@/libs/data-access/klass/models/ClassificationResource';
 import { localization } from '@/libs/language';
 import styles from './classification-page.module.css';
 
-export default function ClassificationDetail({ id }: Readonly<{ id: string }>) {
+export default function ClassificationDetail({ classification }: Readonly<{ classification: ClassificationResource }>) {
   const homeUrl = { text: localization.tabs.classifications, href: `/classifications` };
-  const breadcrumbList = id ? ([{ text: id, href: '' }] as BreadcrumbItem[]) : [];
+  const breadcrumbList = classification ? ([{ text: classification.name, href: '' }] as BreadcrumbItem[]) : [];
 
   return (
     <section className={`${styles.detailsPage} container`}>
       <DataportalBreadcrumbs items={breadcrumbList} homeUrl={homeUrl} />
       <main className={styles.detailsPage}>
+        <Heading className={`${styles.detailsHeading} primaryHeading`} data-size='xl' level={1}>
+          {classification.name}
+        </Heading>
         <Alert data-color={'warning'} className='infoAlert'>
           Detaljside for klassifikasjon er ikke klar for testing.
         </Alert>
