37c062e5dc5387855203134eaf2c331b163760c6
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-07-01 18:06:46 +0200

    lint

--- e2e/classification-accessibility.spec.ts
+++ e2e/classification-accessibility.spec.ts
@@ -5,10 +5,16 @@ import AxeBuilder from '@axe-core/playwright';
 
 const classifications = classificationMock.classifications;
 
-
-test('Classifications details page follow wcag standard', async ({ classificationDetailsPage }) => {
+test('Page has heading rank 1', async ({ classificationDetailsPage }) => {
   const classification = parseClassification(classifications[1]);
   const page = await classificationDetailsPage(classification.id!);
   const results = await new AxeBuilder({ page }).withRules('page-has-heading-one').analyze();
-    expect(results.violations).toEqual([]);
+  expect(results.violations).toEqual([]);
+});
+
+test('Page has correct landmarks', async ({ classificationDetailsPage }) => {
+  const classification = parseClassification(classifications[2]);
+  const page = await classificationDetailsPage(classification.id!);
+  const results = await new AxeBuilder({ page }).withRules('region').analyze();
+  expect(results.violations).toEqual([]);
 });
