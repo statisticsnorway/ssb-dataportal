ee8556f67207ad26b7de47a7e1d85db5e55b0e6e
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-07-01 15:21:20 +0200

    redirect to codes

--- src/app/(details)/classifications/[id]/page.tsx
+++ src/app/(details)/classifications/[id]/page.tsx
@@ -1,24 +1,10 @@
-import { Alert } from '@digdir/designsystemet-react';
+
 import { redirect } from 'next/navigation';
 import { createLogger } from '@/libs/logger/server-logger';
 
-const showInfoOnly = process.env.HIDE_CLASSIFICATIONS === 'true';
-
-const renderInfoOnlyPage = () => {
-  return (
-    <Alert data-color={'warning'} className='infoAlert'>
-      Detaljside for klassifikasjon er ikke klar for testing.
-    </Alert>
-  );
-};
-
 export default async function ClassificationPage({ params }: Readonly<{ params: Promise<{ id: string }> }>) {
+    const { id } = await params;
   const logger = createLogger('classification-details-page');
   logger.info({ params }, 'Classification detail page access');
-  if (showInfoOnly) {
-    logger.info('Classification detail page is running in info-only mode');
-    return renderInfoOnlyPage();
-  }
-  const { id } = await params;
   redirect(`/classifications/${id}/codes`);
 }
