4a962929ac2e78b0afb411d18e27e9207d9ef962
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-07-01 15:21:10 +0200

    render tabs

--- src/app/(details)/classifications/[id]/classificationDetail.tsx
+++ src/app/(details)/classifications/[id]/classificationDetail.tsx
@@ -1,24 +1,64 @@
-import { Alert, Heading } from '@digdir/designsystemet-react';
-import { BreadcrumbItem, DataportalBreadcrumbs } from '@/components/dataportal-breadcrumbs';
+'use client';
+
+import { Divider, Heading, Paragraph, Tabs, Tag } from '@digdir/designsystemet-react';
+import { usePathname, useRouter } from 'next/navigation';
+import { DataportalBreadcrumbs } from '@/components/dataportal-breadcrumbs';
 import { ClassificationResource } from '@/libs/data-access/klass/models/ClassificationResource';
 import { localization } from '@/libs/language';
+import { getHomeBreadcrumb } from '@/utils/breadcrumbs';
 import styles from './classification-page.module.css';
+import { classificationDetailsTabsData, getClassificationDetailsTabForRoute } from './tabs';
 
-export default function ClassificationDetail({ classification }: Readonly<{ classification: ClassificationResource }>) {
-  const homeUrl = { text: localization.tabs.classifications, href: `/classifications` };
-  const breadcrumbList = classification ? ([{ text: classification.name, href: '' }] as BreadcrumbItem[]) : [];
+export default function ClassificationDetail({
+  classification,
+  children,
+}: Readonly<{ classification: ClassificationResource; children: React.ReactNode }>) {
+  const pathname = usePathname();
+  const router = useRouter();
+  const activeTab = getClassificationDetailsTabForRoute(pathname) ?? classificationDetailsTabsData.Codes;
 
+  const validTag = true;
   return (
-    <section className={`${styles.detailsPage} container`}>
-      <DataportalBreadcrumbs items={breadcrumbList} homeUrl={homeUrl} />
-      <main className={styles.detailsPage}>
+    <div className={`${styles.detailsPage} container`}>
+      <DataportalBreadcrumbs
+        homeUrl={getHomeBreadcrumb()}
+        items={[
+          {
+            text: localization.classification.labelPlural,
+            href: `/classifications`,
+          },
+        ]}
+        currentText={classification.name ?? String(classification.id)}
+      />
+      <main className={styles.mainContent}>
         <Heading className={`${styles.detailsHeading} primaryHeading`} data-size='xl' level={1}>
           {classification.name}
         </Heading>
-        <Alert data-color={'warning'} className='infoAlert'>
-          Detaljside for klassifikasjon er ikke klar for testing.
-        </Alert>
+        <Paragraph className={`${styles.description} ingress`}>{classification.description}</Paragraph>
+        <Divider />
+        <Heading className={`${styles.detailsHeading} primaryHeading`} data-size='lg' level={2}>
+          Versjonens navn
+        </Heading>
+        {validTag && <Tag>Er dette dagens versjon?</Tag>}
+        <Tabs value={activeTab.id}>
+          <Tabs.List aria-label={localization.tabs.ariaLabel}>
+            {Object.values(classificationDetailsTabsData).map((tab) => (
+              <Tabs.Tab
+                aria-controls={tab.id}
+                key={tab.id}
+                value={tab.id}
+                className={`${styles.tab} font-roboto`}
+                onClick={() => router.push(`/classifications/${classification.id}/${tab.slug}`)}
+              >
+                {tab.label}
+              </Tabs.Tab>
+            ))}
+          </Tabs.List>
+          <Tabs.Panel value={activeTab.id}>
+            {children}
+          </Tabs.Panel>
+        </Tabs>
       </main>
-    </section>
+    </div>
   );
 }
