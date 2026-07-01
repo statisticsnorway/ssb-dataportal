d6a0c3f8c9c9e23ec8db9c7941bd90d3c8c6e685
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-07-01 23:08:04 +0200

    add button - conditionally render

--- src/app/(details)/classifications/[id]/classificationDetail.tsx
+++ src/app/(details)/classifications/[id]/classificationDetail.tsx
@@ -2,6 +2,7 @@
 
 import { Divider, Heading, Paragraph, Tabs, Tag } from '@digdir/designsystemet-react';
 import { usePathname, useRouter } from 'next/navigation';
+import { SubscribeButton } from '@/app/(details)/classifications/components/subscribe';
 import { DataportalBreadcrumbs } from '@/components/dataportal-breadcrumbs';
 import { ClassificationResource } from '@/libs/data-access/klass/models/ClassificationResource';
 import { localization } from '@/libs/language';
@@ -34,7 +35,8 @@ export default function ClassificationDetail({
         <Heading className={`${styles.detailsHeading} primaryHeading`} data-size='xl' level={1}>
           {classification.name}
         </Heading>
-        <Paragraph className={`${styles.description} ingress`}>{classification.description}</Paragraph>
+        {classification.description && <Paragraph className={`${styles.description} ingress`}>{classification.description}</Paragraph>}
+        <SubscribeButton classificationId={classification.id} />
         <Divider />
         <Heading className={`${styles.detailsHeading} primaryHeading`} data-size='lg' level={2}>
           Versjonens navn
