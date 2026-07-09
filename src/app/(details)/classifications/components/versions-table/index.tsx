5b4df012f21c5ae0564afeb1bce8e8b42ee338bb
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-07-09 09:40:07 +0200

    remove card wrapper

--- src/app/(details)/classifications/components/versions-table/index.tsx
+++ src/app/(details)/classifications/components/versions-table/index.tsx
@@ -1,4 +1,4 @@
-import { Card, Table, TableBody, TableCell, TableHead, TableHeaderCell, TableRow } from '@digdir/designsystemet-react';
+import { Table, TableBody, TableCell, TableHead, TableHeaderCell, TableRow } from '@digdir/designsystemet-react';
 import { ReactNode } from 'react';
 import styles from './versionsTable.module.css';
 
@@ -14,28 +14,24 @@ export interface VersionItem {
 const VersionsTable = ({ content }: VersionsTableProps) => {
   const headers = content[0]?.map((item) => item.label) ?? [];
   return (
-    <Card className={styles.tableContainer}>
-      <Table border={true} zebra={true} hover={true}>
-        <TableHead>
-          <TableRow className={styles.row}>
-            {headers.map((header) => (
-              <TableHeaderCell key={header}>{header}</TableHeaderCell>
+    <Table border={true} zebra={true} hover={true} className={styles.table}>
+      <TableHead>
+        <TableRow className={styles.row}>
+          {headers.map((header) => (
+            <TableHeaderCell key={header}>{header}</TableHeaderCell>
+          ))}
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {content.map((row, index) => (
+          <TableRow key={index} className={styles.row}>
+            {row.map((item, i) => (
+              <TableCell key={i}>{item.value instanceof Date ? item.value.toLocaleDateString() : item.value}</TableCell>
             ))}
           </TableRow>
-        </TableHead>
-        <TableBody>
-          {content.map((row, index) => (
-            <TableRow key={index} className={styles.row}>
-              {row.map((item, i) => (
-                <TableCell key={i}>
-                  {item.value instanceof Date ? item.value.toLocaleDateString() : item.value}
-                </TableCell>
-              ))}
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Card>
+        ))}
+      </TableBody>
+    </Table>
   );
 };
 
