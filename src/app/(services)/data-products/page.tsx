88bdf1efbcfbe305e922e989d84d12ea57e70199
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2026-07-31 09:38:13 +0200

    Pipe language choice for subject fields

--- src/app/(services)/data-products/page.tsx
+++ src/app/(services)/data-products/page.tsx
@@ -1,7 +1,8 @@
 import { Metadata } from 'next';
+import { cookies, headers } from 'next/headers';
 import { fetchSubjectFieldFilterValues } from '@/libs/data/classifications/codesData';
 import { listDataProducts } from '@/libs/data/datasets/datasets';
-import { localization } from '@/libs/language';
+import { languageCookieName, localization, resolveLanguage } from '@/libs/language';
 import { createLogger } from '@/libs/logger/server-logger';
 import { DataProductsServicePage } from './data-products-service-page';
 
@@ -15,9 +16,15 @@ export default async function DataProducts({
   searchParams: Promise<Record<string, string | string[] | undefined>>;
 }>) {
   const params = await searchParams;
+  const cookieStore = await cookies();
+  const requestHeaders = await headers();
+  const language = resolveLanguage(
+    cookieStore.get(languageCookieName)?.value,
+    requestHeaders.get('accept-language') ?? undefined,
+  );
   const logger = createLogger('data-products-discover-page');
   logger.info({ params }, 'Data products page access');
   const dataProducts = await listDataProducts();
-  const subjectFields = await fetchSubjectFieldFilterValues();
+  const subjectFields = await fetchSubjectFieldFilterValues(language);
   return <DataProductsServicePage dataProducts={dataProducts} subjectFields={subjectFields} />;
 }
