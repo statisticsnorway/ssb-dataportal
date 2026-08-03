770024bab3bb539d1aa16c7e985314f934362fff
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-08-03 10:32:17 +0200

    add title and description

--- src/app/(details)/classifications/components/views/AboutView.tsx
+++ src/app/(details)/classifications/components/views/AboutView.tsx
@@ -1,3 +1,19 @@
-export default async function AboutView() {
-  return <div>Om</div>;
+import { Heading, Paragraph } from '@digdir/designsystemet-react';
+import { ClassificationResource } from '@/libs/data-access/klass/models';
+
+interface AboutViewProps {
+  classification: ClassificationResource;
+}
+export default async function AboutView({ classification }: AboutViewProps) {
+  return (
+    <div>
+      <Heading>{classification.name}</Heading>
+      <Paragraph>{classification.description}</Paragraph>
+      {/*
+       * Key - value table
+        Levels - table
+        Changelog - table
+       */}
+    </div>
+  );
 }
