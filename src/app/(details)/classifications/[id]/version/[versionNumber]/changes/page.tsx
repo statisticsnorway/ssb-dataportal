c0492c7fc65f85e68f92bb849910ad0658405cbb
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2026-08-05 10:22:59 +0200

    Static skeleton

--- src/app/(details)/classifications/[id]/version/[versionNumber]/changes/page.tsx
+++ src/app/(details)/classifications/[id]/version/[versionNumber]/changes/page.tsx
@@ -1,5 +1,15 @@
+'use client';
+
+import { notFound } from 'next/navigation';
+import { useVersion } from '@/app/(details)/classifications/components/versionContext';
 import ChangesView from '@/app/(details)/classifications/components/views/ChangesView';
 
-export default async function Changes() {
+export default function ChangesVersion() {
+  const { versionResource } = useVersion();
+
+  if (!versionResource?.classificationItems) {
+    return notFound();
+  }
+
   return <ChangesView />;
 }
--- src/app/(details)/classifications/components/views/ChangesView.tsx
+++ src/app/(details)/classifications/components/views/ChangesView.tsx
@@ -1,3 +1,34 @@
-export default async function ChangesView() {
-  return <div>Endringer</div>;
+import { Table, TableBody, TableCell, TableHead, TableHeaderCell, TableRow } from '@digdir/designsystemet-react';
+import styles from './views.module.css';
+
+const content = [
+  [
+    { label: '0202 this code', value: '0202 this code' },
+    { label: '0201 that code', value: '0201 that code' },
+  ],
+];
+
+export default function ChangesView() {
+  // Work out from and to dates based on relevant versions
+  // Get changes from endpoint e.g. http https://data.ssb.no/api/klass/v1/classifications/131/changes\?from\=2025-01-01
+  // Shall we filter "identical" changes out?
+  return (
+    <Table border={true} zebra={true} hover={true} className={styles.table}>
+      <TableHead>
+        <TableRow>
+          <TableHeaderCell key='Version 1'>Version 1</TableHeaderCell>
+          <TableHeaderCell key='Version 2'>Version 2</TableHeaderCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {content.map((row) => (
+          <TableRow key={row.map((item) => item.value).join('-')}>
+            {row.map((item) => (
+              <TableCell key={item.label}>{item.value}</TableCell>
+            ))}
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
 }
