f472bcb47d676f219e9769e03c16ab1450e7edcd
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-08-06 09:17:23 +0200

    re install v8, instanbul requires more set up

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './vitest.setup.ts',
     coverage: {
-      provider: 'istanbul',
+      provider: 'v8',
       reporter: ['text', 'html', 'lcov'],
       enabled: true,
       reportsDirectory: './coverage',
