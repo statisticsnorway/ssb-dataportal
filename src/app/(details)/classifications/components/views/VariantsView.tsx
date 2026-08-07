f41456237557f18c2d33ab20598edb2c1cd3474e
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-08-07 13:06:35 +0200

    use alerts component

--- src/app/(details)/classifications/components/views/VariantsView.tsx
+++ src/app/(details)/classifications/components/views/VariantsView.tsx
@@ -1,4 +1,4 @@
-import { Heading } from '@digdir/designsystemet-react';
+import { Alert, Heading } from '@digdir/designsystemet-react';
 import { ClassificationVersionResource } from '@/libs/data-access/klass';
 import { localization } from '@/libs/language/src/localization';
 import { formatVariantName, mapVariantItems } from '../../utils/variants';
@@ -19,13 +19,15 @@ export default function VariantsView({ classificationVersion }: Readonly<Variant
         </Heading>
         <p>{localization.classification.variant.variantInfo}</p>
       </header>
-      {variants.map((variant) => (
-        <ClassificationCard
-          title={formatVariantName(variant.name)}
-          content={mapVariantItems(variant)}
-          key={variant.id}
-        />
-      ))}
+      {variants.length > 0 &&
+        variants.map((variant) => (
+          <ClassificationCard
+            title={formatVariantName(variant.name)}
+            content={mapVariantItems(variant)}
+            key={variant.id}
+          />
+        ))}
+      {variants.length === 0 && <Alert data-color='info'>{localization.classification.variant.noVariants}</Alert>}
     </div>
   );
 }
