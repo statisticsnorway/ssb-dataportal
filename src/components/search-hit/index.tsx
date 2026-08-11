52cf43db0b3a3c10f1f3c2707e483317c0dedf04
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-08-11 13:01:34 +0200

    add Tag

--- src/components/search-hit/index.tsx
+++ src/components/search-hit/index.tsx
@@ -1,4 +1,5 @@
-import { Card, Heading, Link, Paragraph } from '@digdir/designsystemet-react';
+import { Card, Heading, Link, Paragraph, Tag } from '@digdir/designsystemet-react';
+import { InformationSquareIcon } from '@navikt/aksel-icons';
 import { ReactNode } from 'react';
 import styles from './search-hit.module.css';
 
@@ -8,10 +9,20 @@ interface SearchHitProps {
   description?: string;
   tagsList?: ReactNode;
 }
-
+// const resolveLanguage prop
+// has not value in selected language
+const hasNotValueInSelectedLanguage = true;
 const SearchHit = ({ title, href, description, tagsList }: SearchHitProps) => {
   return (
     <Card aria-label={title} role='article'>
+      {hasNotValueInSelectedLanguage && (
+        <div className={styles.hasNotValueInSelectedLanguage}>
+          <Tag data-size='lg' className={styles.languageTag}>
+            <InformationSquareIcon />
+            Engelsk
+          </Tag>
+        </div>
+      )}
       {href && title ? (
         <Heading data-size='md' className={styles.headingLink} level={2}>
           <Link href={href}>{title && <span className='primaryHeading'>{title}</span>}</Link>
