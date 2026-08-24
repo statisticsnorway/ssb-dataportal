59767e52f639206f7760e147e81d13d7ada7ed99
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2026-08-24 11:08:38 +0200

    Correspondences from/to

--- src/app/(details)/classifications/utils/correspondences.ts
+++ src/app/(details)/classifications/utils/correspondences.ts
@@ -5,9 +5,9 @@ import { Item } from '@/types/item';
 
 export const mapCorrespondenceItems = (table: CorrespondenceTableSummaryResource): Item[] => {
   const rows: Item[] = [];
-  addRow(rows, localization.classification.correspondence.from, table.source);
+  addRow(rows, localization.from, table.source);
   addRow(rows, localization.classification.correspondence.fromLevel, table.sourceLevel?.levelName);
-  addRow(rows, localization.classification.correspondence.to, table.target);
+  addRow(rows, localization.to, table.target);
   addRow(rows, localization.classification.correspondence.toLevel, table.targetLevel?.levelName);
   addRow(rows, localization.classification.correspondence.owner, table.owningSection);
   return rows;
