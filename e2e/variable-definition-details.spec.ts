7919a9df21b114c423aeeb5c186b8575d19e568c
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-08-25 16:14:56 +0200

    correct name

--- e2e/variable-definition-details.spec.ts
+++ e2e/variable-definition-details.spec.ts
@@ -174,7 +174,7 @@ test.describe('Contact', () => {
 
   test('Vardef API documentation link points to the internal test docs URL when authenticated', async ({ page }) => {
     await goToDetail(page);
-    const apiLink = page.getByRole('link', { name: localization.apiDocVardef });
+    const apiLink = page.getByRole('link', { name: localization.apiDocumentation });
     await expect(apiLink).toHaveAttribute(
       'href',
       'https://metadata.test.ssb.no/docs/swagger/variable-definitions?urls.primaryName=internal',
