b5dfecbd7f22431ebe36ae3315170ed89f21b2e3
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-09-08 13:32:52 +0200

    add language for changelogs

--- src/app/(details)/classifications/components/expandable-table/index.tsx
+++ src/app/(details)/classifications/components/expandable-table/index.tsx
@@ -1,4 +1,7 @@
-import { Card, Details, DetailsSummary, Paragraph } from '@digdir/designsystemet-react';
+import { Card, Details, DetailsSummary, Paragraph, Tag, Tooltip } from '@digdir/designsystemet-react';
+import { GlobeIcon } from '@navikt/aksel-icons';
+import { localization } from '@/libs/language';
+import { formatLanguages } from '@/utils/functions';
 import styles from './expandableTable.module.css';
 
 interface ExpandableTableProps {
@@ -7,12 +10,20 @@ interface ExpandableTableProps {
   title?: string;
 }
 const ExpandableTable = ({ table, title, message }: ExpandableTableProps) => {
+  const languageTag = (
+    <Tooltip content={localization.classification.language.notSelectedLanguage}>
+      <Tag data-size='lg' tabIndex={0}>
+        <GlobeIcon aria-hidden='true' focusable='false' />
+        {formatLanguages('nb')}
+      </Tag>
+    </Tooltip>
+  );
   return (
     <Card className={styles.card}>
       <Details className={styles.details}>
-        {title && <DetailsSummary>{title}</DetailsSummary>}
+        {title && <DetailsSummary>{title} {languageTag}</DetailsSummary>}
         {table && <span className={styles.table}>{table}</span>}
-        {message && <Paragraph>{message}</Paragraph>}
+        {message && <Paragraph lang='no'>{message}</Paragraph>}
       </Details>
     </Card>
   );
