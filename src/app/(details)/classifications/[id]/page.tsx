680fd5eb7e8268965af8783dc757148f19f90186
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-09-11 14:18:43 +0200

    use exclusive labels mabl

--- src/app/(details)/classifications/[id]/page.tsx
+++ src/app/(details)/classifications/[id]/page.tsx
@@ -5,6 +5,6 @@ import { createLogger } from '@/libs/logger/server-logger';
 export default async function ClassificationPage({ params }: Readonly<{ params: Promise<{ id: string }> }>) {
   const { id } = await params;
   const logger = createLogger('classification-details-page');
-  logger.info({ id }, 'Classification detail page access - test trigger 3');
+  logger.info({ id }, 'Classification detail page access - test trigger 4');
   redirect(buildUrl({ classificationId: Number(id), tab: 'codes' }));
 }
