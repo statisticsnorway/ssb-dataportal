506b53512799e0b28e848c9f045e1b8c0813d4af
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2026-09-16 13:26:48 +0200

    Reduce number of unnecessary logs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,14 @@ import { AuthProvider } from './authContext';
 
 const logger = createLogger('app:root');
 
+logger.info(
+  {
+    useStaticData: process.env.VARDEF_USE_STATIC_DATA === 'true',
+    nodeEnv: process.env.NODE_ENV,
+  },
+  'Application initializing',
+);
+
 export const generateMetadata = async (): Promise<Metadata> => {
   const cookieStore = await cookies();
   const requestHeaders = await headers();
@@ -40,15 +48,6 @@ const RootLayout = async ({ children }: { children: React.ReactNode }) => {
   );
 
   localization.setLanguage(language);
-
-  logger.info(
-    {
-      language,
-      useStaticData: process.env.VARDEF_USE_STATIC_DATA === 'true',
-      nodeEnv: process.env.NODE_ENV,
-    },
-    'Application initializing',
-  );
 
   const authResult = await authenticateUser();
 
